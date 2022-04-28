6cd9ef1f023cb16c4f574133d1a5b6542a071fda
sigristarisa <arisasigrist.ch@gmail.com>
2022-04-28 19:00:35 +0200

    cleaned code

--- src/createBreweryUl.js
+++ src/createBreweryUl.js
@@ -1,11 +1,11 @@
 import createBreweryLi from "./createBreweryLi.js";
 
-const createBreweryUl = (targetBreweries) => {
+const createBreweryUl = (breweries) => {
   const breweryUl = document.querySelector("#breweries-list");
   breweryUl.innerHTML = "";
   const breweryTypes = ["micro", "regional", "brewpub"];
 
-  targetBreweries.forEach((brewery) => {
+  breweries.forEach((brewery) => {
     if (breweryTypes.includes(brewery.brewery_type))
       breweryUl.append(createBreweryLi(brewery));
   });
