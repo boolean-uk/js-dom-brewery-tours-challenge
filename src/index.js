e3c82c4c8ee2fc4986dcc2e19fa240cc34bd4cdc
Toby Carlo <tobycarlo.c@gmail.com>
2022-04-28 16:56:27 +0100

    rendering breweries

--- src/index.js
+++ src/index.js
@@ -32,3 +32,26 @@ function clear() {
   breweriesList.innerHTML = "";
   formInputEl.value = "";
 }
+
+function renderBreweries() {
+  for (const brewery of state.breweries) {
+    console.log(brewery);
+    const breweryListEl = document.createElement("li");
+    console.log(breweriesList);
+    breweryListEl.innerHTML = `<h2>${brewery.name}</h2>
+        <div class="type">${brewery.brewery_type}</div>
+        <section class="address">
+            <h3>Address:</h3>
+            <p>${brewery.street}</p>
+            <p><strong>${brewery.city}, ${brewery.postal_code}</strong></p>
+        </section>
+        <section class="phone">
+            <h3>Phone:</h3>
+            <p>${brewery.phone}</p>
+        </section>
+        <section class="link">
+            <a href=${brewery.website_url} target="_blank">Visit Website</a>
+        </section>`;
+    breweriesList.append(breweryListEl);
+  }
+}
