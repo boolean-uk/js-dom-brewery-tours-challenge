988a2af6e24ae8f37be7677ede7d8cdaab308bca
Toby Carlo <tobycarlo.c@gmail.com>
2022-04-28 16:30:08 +0100

    adding eventListeners & search values

--- src/index.js
+++ src/index.js
@@ -6,3 +6,29 @@ let url = "https://api.openbrewerydb.org/breweries?by_state=";
 
 const formEl = document.querySelector("#select-state-form");
 const formInputEl = document.querySelector("#select-state");
+
+formEl.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const inputSearchValue = formInputEl.value;
+  console.log(inputSearchValue);
+  fetch(url + inputSearchValue)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (breweries) {
+      state.breweries = breweries;
+      console.log(state.breweries);
+      render();
+    });
+});
+
+function render() {
+  clear();
+  renderBreweries();
+}
+
+function clear() {
+  breweriesList.innerHTML = "";
+  formInputEl.value = "";
+}
