34b328afbeeaa6b6eb8439101ecf94c8c61e8cd5
sigristarisa <arisasigrist.ch@gmail.com>
2022-04-28 18:02:32 +0200

    finished main criteria

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import getRequest from "./getRequest.js";
 
 const selectStateForm = document.querySelector("#select-state-form");
 const filterByType = document.querySelector("#filter-by-type");
-const selectStateText = document.querySelector("#select-state");
+let targetState = "";
 
 selectStateForm.addEventListener("submit", (e) => {
   const stateInput = e.target[0].value;
   e.preventDefault();
   getRequest(stateInput);
+  targetState = stateInput;
   e.target.reset();
 });
 
 filterByType.addEventListener("change", (e) => {
   const targetBreweryType = e.target.value;
+  getRequest(targetState, targetBreweryType);
 });
