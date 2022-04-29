ce56e24339aaf1e48c0638f47ea6a3ab69ef1887
Paulo Barata <pbara7a@gmail.com>
2022-04-29 10:31:53 +0100

    FIX: Renders only breweries from wanted types when filtering by city.

--- src/render.js
+++ src/render.js
@@ -4,20 +4,21 @@ import state from "./state.js";
 
 function render(data) {
   const breweriesList = document.getElementById('breweries-list')
+  const targetBreweryTypes = ['micro', 'regional', 'brewpub']
 
   const filterFlag = Boolean([...state.cityFilters].length)
   if (filterFlag) { // Render option if there are filters checked
   breweriesList.innerHTML = ''
     data.forEach(element => {
-      const { city } = element
-      if (state.cityFilters.has(city)) breweriesList.append(breweryLi(element))
+      const { city, brewery_type: type } = element
+      if (state.cityFilters.has(city) && targetBreweryTypes.includes(type)) {
+        breweriesList.append(breweryLi(element))
+      }
     })
     return
   }
 
-  const targetBreweryTypes = ['micro', 'regional', 'brewpub']
   breweriesList.innerHTML = ''
-
   data.forEach(element => { // Normal rendering
     const { brewery_type: type } = element
     if (targetBreweryTypes.includes(type)) breweriesList.append(breweryLi(element))
