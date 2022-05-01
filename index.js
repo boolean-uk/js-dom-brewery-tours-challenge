4506453ef2acdeea0c29839069f125b19c281856
AlexDjangoX <alexmonk17@gmail.com>
2022-05-01 18:46:06 +0200

    pagination and some refactoring done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const state = {
   data: [],
   cityList: [],
   selectedCityList: [],
+  pageNumber: 1,
 };
 
 const brewList = document.getElementById("breweries-list");
@@ -12,6 +13,49 @@ const breweryTypes = ["micro", "regional", "brewpub"];
 const filterByCityForm = document.getElementById("filter-by-city-form");
 const searchBreweriesForm = document.getElementById("search-breweries-form");
 const clearAllCheckboxes = document.querySelector(".clear-all-btn");
+
+const firstPage = document.querySelector(".first");
+const nextPage = document.querySelector(".next");
+const previousPage = document.querySelector(".previous");
+
+firstPage.addEventListener("click", (event) => {
+  state.pageNumber = 1;
+  console.log("firstPage", state.pageNumber);
+  console.log(state.searchState);
+  fetch(
+    `https://api.openbrewerydb.org/breweries?by_state=${state.searchState}&per_page=10`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      renderList(data);
+    });
+});
+
+nextPage.addEventListener("click", () => {
+  const breweryListItems = document.querySelectorAll(".brewery-list-item");
+  if (breweryListItems.length > 1) state.pageNumber += 1;
+  fetchData().then((data) => {
+    state.data = data;
+    renderList(data);
+
+    if (breweryListItems.length === 0) {
+      brewList.insertAdjacentHTML(
+        "afterbegin",
+        `<h3>Oops.....that was the last brewery. Go back a page</h3>`
+      );
+    }
+  });
+});
+
+previousPage.addEventListener("click", () => {
+  if (state.pageNumber > 1) state.pageNumber -= 1;
+  console.log("previous", state.pageNumber);
+  fetchData().then((data) => {
+    state.data = data;
+    renderList(data);
+  });
+});
+
 clearAllCheckboxes.addEventListener("click", (event) => {
   clearCheckedBoxes();
 });
@@ -30,7 +74,7 @@ searchForm.addEventListener("submit", (event) => {
   event.preventDefault();
   let searchInput = event.target.querySelector("#select-state").value;
   state.searchState = searchInput.toLowerCase().replace(" ", "_");
-  // searchForm.reset();
+  searchForm.reset();
   fetchData().then((data) => {
     state.data = data;
     renderList(data);
@@ -52,21 +96,9 @@ function createUrl() {
   if (state.searchState === "") return "";
 
   if (state.searchState) {
-    let url = `https://api.openbrewerydb.org/breweries?by_state=${state.searchState}`;
-
-    switch (state.breweryType) {
-      case "micro":
-        url = url + `&by_type=micro`;
-        break;
-      case "regional":
-        url = url + `&by_type=regional`;
-
-        break;
-      case "brewpub":
-        url = url + `&by_type=brewpub`;
-
-        break;
-    }
+    let url = `https://api.openbrewerydb.org/breweries?by_state=${state.searchState}&per_page=10&page=${state.pageNumber}`;
+    if (state.breweryType)
+      url = url + `&by_type=${state.breweryType.toLowerCase()}`;
     return url;
   } else if (state.byName) {
     console.log(state.byName);
@@ -91,22 +123,8 @@ function renderList(data) {
 
   data.forEach((el) => {
     if (breweryTypes.includes(el.brewery_type)) {
-      const htmlBrewery = `<li>
-        <h2>${el.name}</h2>
-        <div class="type">${el.brewery_type}</div>
-        <section class="address">
-          <h3>Address:</h3>
-          <p>${el.street}</p>
-          <p><strong>${el.city}, ${el.postal_code}</strong></p>
-        </section>
-        <section class="phone">
-          <h3>Phone:</h3>
-          <p>N/A</p>
-        </section>
-        <section class="link">
-          <a href="${el.website_url}" target="_blank">Visit Website</a>
-        </section>
-        </li>`;
+      const htmlBrewery = getBreweryHtml(el);
+
       brewList.insertAdjacentHTML("afterbegin", htmlBrewery);
 
       if (!cityList.includes(el.city)) {
@@ -120,6 +138,8 @@ function renderList(data) {
 
 function renderCityList(cityList) {
   filterByCityForm.innerHTML = "";
+
+  state.selectedCityList = [];
 
   cityList.forEach((city) => {
     const htmlCityList = `<input class='city-list' id='${city}item-list' type="checkbox" name="${city.toLowerCase()}" value="${city.toLowerCase()}" />
@@ -155,27 +175,13 @@ function toggleSelectedCities(event) {
   if (state.selectedCityList.length === 0) renderList(state.data);
 }
 
-function renderFromArrayNames(namesArray) {
+function renderFromArrayNames() {
   brewList.innerHTML = "";
 
   state.data.forEach((el) => {
     if (state.selectedCityList.includes(el.city.toLowerCase())) {
-      const htmlBrewery = `<li>
-        <h2>${el.name}</h2>
-        <div class="type">${el.brewery_type}</div>
-        <section class="address">
-          <h3>Address:</h3>
-          <p>${el.street}</p>
-          <p><strong>${el.city}, ${el.postal_code}</strong></p>
-        </section>
-        <section class="phone">
-          <h3>Phone:</h3>
-          <p>N/A</p>
-        </section>
-        <section class="link">
-          <a href="${el.website_url}" target="_blank">Visit Website</a>
-        </section>
-        </li>`;
+      const htmlBrewery = getBreweryHtml(el);
+
       brewList.insertAdjacentHTML("afterbegin", htmlBrewery);
     }
   });
@@ -187,4 +193,25 @@ function clearCheckedBoxes() {
     city.checked = false;
   });
   renderList(state.data);
+}
+
+function getBreweryHtml(el) {
+  const htmlBrewery = `<li class='brewery-list-item'>
+  <h2>${el.name}</h2>
+  <div class="type">${el.brewery_type}</div>
+  <section class="address">
+    <h3>Address:</h3>
+    <p>${el.street}</p>
+    <p><strong>${el.city}, ${el.postal_code}</strong></p>
+  </section>
+  <section class="phone">
+    <h3>Phone:</h3>
+    <p>N/A</p>
+  </section>
+  <section class="link">
+    <a href="${el.website_url}" target="_blank">Visit Website</a>
+  </section>
+  </li>`;
+
+  return htmlBrewery;
 }
