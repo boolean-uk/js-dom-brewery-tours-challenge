ae3df247e0f6b68c68b5d19fb333d71c30bc6062
Dan <dannorthcott8@gmail.com>
2022-05-03 09:43:22 +0100

    big refactor - extension 2 half working

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const filterByTypeForm = document.getElementById("filter-by-type-form");
 const searchBreweriesForm = document.getElementById("search-breweries-form");
 const filterByCityForm = document.getElementById("filter-by-city-form");
 
+// ---------------------- state ----------------------
 const state = {
   state: "",
+
+  breweries: [],
+  breweriesFiltered: [],
+
   breweryType: "",
   breweryName: "",
   desiredCities: [],
@@ -13,103 +18,148 @@ const state = {
 
 const validBreweryTypes = ["micro", "regional", "brewpub"];
 
+// ---------------------- main event Listners ----------------------
+
 selectStateForm.addEventListener("submit", (e) => {
   e.preventDefault();
   state.state = e.target[0].value;
-  getAndRenderBreweries();
+  getBreweries();
 });
 
 filterByTypeForm.addEventListener("change", (e) => {
   e.preventDefault();
   state.breweryType = e.target.value;
-  getAndRenderBreweries();
+  _filterBreweries();
 });
 
 searchBreweriesForm.addEventListener("input", (e) => {
-  state.breweryName = e.target.value;
-  getAndRenderBreweries();
+  state.breweryName = "";
+  state.breweryName += e.target.value;
+  _filterBreweries();
 });
 
-function getAndRenderBreweries() {
-  fetch(_createURL())
+// ---------------------- GET method ----------------------
+
+function getBreweries() {
+  fetch(`https://api.openbrewerydb.org/breweries?by_state=${state.state}`)
     .then((res) => res.json())
     .then((data) => {
-      const filtered = data.filter((brewery) => {
+      const validBreweries = data.filter((brewery) => {
         if (validBreweryTypes.includes(brewery.brewery_type)) return true;
       });
-      _renderCityCheckBoxes(filtered);
-      _renderBreweries(filtered);
+      state.breweries = validBreweries;
+
+      _renderBreweries(validBreweries);
     });
 }
 
-function _createURL() {
-  return `https://api.openbrewerydb.org/breweries?by_state=${state.state}${
-    state.breweryType ? "&by_type=" + state.breweryType : ""
-  }${state.breweryName ? "&by_name=" + state.breweryName : ""}`;
+// ---------------------- filtering of breweries ----------------------
+
+function _filterBreweries() {
+  state.breweriesFiltered = state.breweries;
+  _filterByType();
+  _filterByName();
+  _filterByCity();
+  _renderBreweries(state.breweriesFiltered);
 }
+
+function _filterByType() {
+  if (state.breweryType.length > 0) {
+    state.breweriesFiltered = state.breweries.filter(
+      (brewery) => brewery.brewery_type === state.breweryType
+    );
+  }
+}
+
+function _filterByName() {
+  if (!state.breweryName) _renderBreweries(state.breweries);
+  else {
+    state.breweriesFiltered = state.breweriesFiltered.filter((brewery) =>
+      brewery.name.toLowerCase().includes(state.breweryName.toLowerCase())
+    );
+  }
+}
+
+function _filterByCity() {
+  console.log(state.breweriesFiltered);
+  state.breweriesFiltered = state.breweriesFiltered.filter((brewery) =>
+    state.desiredCities.includes(brewery.city)
+  );
+  console.log(state.breweriesFiltered);
+}
+
+// ---------------------- rendering of Page elements----------------------
 
 function _renderCityCheckBoxes(breweries) {
   filterByCityForm.innerHTML = "";
   const cityNameArray = [...new Set(breweries.map((brewery) => brewery.city))];
 
-  cityNameArray.forEach((cityName) => {
-    const input = document.createElement("input");
-    input.type = "checkbox";
-    input.name = cityName;
-    input.value = cityName;
-
-    const label = document.createElement("label");
-    label.for = cityName;
-    label.innerText = cityName;
-
-    filterByCityForm.append(input);
-    filterByCityForm.append(label);
-
-    input.addEventListener("change", (e) =>
-      _filterByCity(breweries, e.target.value)
-    );
-  });
-}
-
-function _filterByCity(breweries, breweryName) {
-  state.desiredCities.push(breweryName);
-
-  const filtered = breweries.filter((brewery) => {
-    if (state.desiredCities.includes(brewery.city)) return true;
-  });
-
-  _renderBreweries(filtered);
-  console.log(filtered);
+  cityNameArray.forEach((cityName) => _createCityCheckBox(cityName));
 }
 
 function _renderBreweries(breweries) {
+  _renderCityCheckBoxes(state.breweries);
+
   breweriesList.innerHTML = "";
 
   breweries.forEach((brewery) => {
-    const li = _createTodoListItem(brewery);
+    const li = _createBreweryItem(brewery);
 
     breweriesList.append(li);
   });
 }
 
-function _createTodoListItem(brewery) {
+// ---------------------- creation of Page elements----------------------
+
+function _createBreweryItem(brewery) {
   const li = document.createElement("li");
 
   li.innerHTML = `
-    <h2>${brewery.name}</h2>
-    <div class="type">${brewery.brewery_type}</div>
-    <section class="address">
-        <h3>Address:</h3>
-        <p>${brewery.street}</p>
-        <p><strong>${brewery.state}, ${brewery.postal_code}</strong></p>
-    </section>
-    <section class="phone">
-        <h3>Phone:</h3>
-        <p>${brewery.phone ? brewery.phone : "N/A"}</p>
-    </section>
-    <section class="link">
-        <a href="${brewery.website_url}" target="_blank">Visit Website</a>
-    </section>`;
+  <h2>${brewery.name}</h2>
+  
+  <div class="type">${brewery.brewery_type}</div>
+  
+  <section class="address">
+  <h3>Address:</h3>
+  <p>${brewery.street}</p>
+  <p><strong>${brewery.state}, ${brewery.postal_code}</strong></p>
+  </section>
+  
+  <section class="phone">
+  <h3>Phone:</h3>
+  <p>${brewery.phone ? brewery.phone : "N/A"}</p>
+  </section>
+  
+  <section class="link">
+  <a href="${brewery.website_url}" target="_blank">Visit Website</a>
+  </section>`;
 
   return li;
+}
+
+function _createCityCheckBox(cityName) {
+  const input = document.createElement("input");
+  input.type = "checkbox";
+  input.name = cityName;
+  input.value = cityName;
+
+  input.addEventListener("change", (e) => {
+    const targetCity = e.target.value;
+
+    if (state.desiredCities.includes(targetCity)) {
+      const idx = state.desiredCities.indexOf(targetCity);
+      state.desiredCities.splice(idx, 1);
+    } else {
+      state.desiredCities.push(targetCity);
+    }
+
+    _filterBreweries();
+  });
+
+  const label = document.createElement("label");
+  label.for = cityName;
+  label.innerText = cityName;
+
+  filterByCityForm.append(input);
+  filterByCityForm.append(label);
 }
