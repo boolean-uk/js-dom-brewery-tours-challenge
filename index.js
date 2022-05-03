39ef2d9c3acdbbf42120ba68cb813c639d69dc74
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2022-05-03 10:36:58 +0100

    update 3

--- index.js
+++ index.js
@@ -1,24 +1,73 @@
 const listUl = document.querySelector("#breweries-list");
+const filterByType = document.querySelector("#filter-by-type")
+const type = document.querySelector('.type')
+
+let breweriesList = []
 
 function renderBreweryApi() {
-    listUl.innerHTML = "";
+    // listUl.innerHTML = "";
     const city = 'ohio'
     // get data
     fetch(`https://api.openbrewerydb.org/breweries?by_state=${city}`)
     .then((res) => res.json())
     .then((data) => {
         // update the page
-        console.log('have data', data)
-        data.forEach((breweryData) => {
-            
-            listUl.append(renderBreweriesList(breweryData));
-        })
+        console.log('have city data', data)
+        renderBreweriesList(data)
+        // breweriesList = data
     })
 }
 
 
+function renderBreweriesList(ListOfBreweries) {
+    listUl.innerHTML = "";
+    ListOfBreweries.forEach((breweryData) => {
+            
+        listUl.append(renderBrewery(breweryData));
+    })
+}
 
-function renderBreweriesList(brewery) {
+function filterBreweries(typeFilter) {
+    // let filteredList = []
+
+    const city = 'ohio'
+    
+    let url = `https://api.openbrewerydb.org/breweries?by_state=${city}`
+
+    //  if (typeFilter === breweriesList.breweryType) {
+        // filteredList = breweriesList.filter(breweriesList.breweryType)
+        url = url + `&by_type=${typeFilter}`
+        // console.log(url)
+        // fetch the url with that type filter then use that new data
+        fetch(url)
+        .then((res) => res.json())
+        .then((filteredData) => {
+            // update the page
+            console.log('have filtered data', filteredData)
+            renderBreweriesList(filteredData)
+        })
+    //  }
+  
+    // for each brewery in breweriesList
+    // if brewery.type === currentBreweryTypeFilterValue -> add to the filtered List
+    // if (filteredList = []) {
+         
+    //  }
+    //  renderBreweriesList(filteredList)
+}
+    
+    filterByType.addEventListener('change', (event) =>{
+        event.preventDefault()
+        // use mdn change event
+        // for ( let i = )
+        // new value of the dropdown
+        const currentBreweryTypeFilterValue =  event.target.value
+        // the value goes into type filter function
+        filterBreweries(currentBreweryTypeFilterValue)
+    })
+
+
+function renderBrewery(brewery) {
     // li
     const li = document.createElement('li')
     // name of the brewery
@@ -27,7 +76,7 @@ function renderBreweriesList(brewery) {
 
     const breweryType = document.createElement('div')
     breweryType.className = 'type'
-    brewery.innerText = brewery.brewery_type
+    breweryType.innerText = brewery.brewery_type
 
     // section element created with the address of the brewery (with title and street) embedded in it
     const brewerySection = document.createElement('section')
@@ -78,5 +127,8 @@ function renderBreweriesList(brewery) {
     )
     return li
 }
+
+
+
 
 renderBreweryApi()
