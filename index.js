066ac3f66d65069567754a2c1b0bfbf0551d3a01
Marin Balabanov <marincomics@gmail.com>
2022-09-12 14:26:03 +0200

    Restricts to micro, regional and brewpub

--- index.js
+++ index.js
@@ -25,55 +25,58 @@ function getBreweries(firstCategory, firstTerm, secondCategory, secondTerm) {
 }
 
 function renderBreweryListItems(singleBrewery) {
-    const BREWERY_LI = document.createElement('li')
+    if (singleBrewery.brewery_type === 'micro' ||
+    singleBrewery.brewery_type === 'regional' ||
+    singleBrewery.brewery_type === 'brewpub' ) {
+        const BREWERY_LI = document.createElement('li')
 
-    const BREWERY_H2 = document.createElement('h2')
-    BREWERY_H2.innerText = singleBrewery.name
+        const BREWERY_H2 = document.createElement('h2')
+        BREWERY_H2.innerText = singleBrewery.name
 
-    const BREWERY_TYPE_DIV = document.createElement('div')
-    BREWERY_TYPE_DIV.setAttribute('class', 'type')
-    BREWERY_TYPE_DIV.innerText = singleBrewery.brewery_type
+        const BREWERY_TYPE_DIV = document.createElement('div')
+        BREWERY_TYPE_DIV.setAttribute('class', 'type')
+        BREWERY_TYPE_DIV.innerText = singleBrewery.brewery_type
 
-    const BREWERY_ADDRESS_SECTION = document.createElement('section')
-    const BREWERY_ADDRESS_STREET = document.createElement('p')
-    BREWERY_ADDRESS_STREET.innerText = singleBrewery.street
-    BREWERY_ADDRESS_SECTION.appendChild(BREWERY_ADDRESS_STREET)
+        const BREWERY_ADDRESS_SECTION = document.createElement('section')
+        const BREWERY_ADDRESS_STREET = document.createElement('p')
+        BREWERY_ADDRESS_STREET.innerText = singleBrewery.street
+        BREWERY_ADDRESS_SECTION.appendChild(BREWERY_ADDRESS_STREET)
 
-    const BREWERY_ADDRESS_CITY_PC = document.createElement('p')
-    const BREWERY_ADDRESS_CITY_PC_STRONG = document.createElement('strong')
-    BREWERY_ADDRESS_CITY_PC_STRONG.innerText = `${singleBrewery.city}, ${singleBrewery.postal_code}, ${singleBrewery.state}`
-    BREWERY_ADDRESS_CITY_PC.appendChild(BREWERY_ADDRESS_CITY_PC_STRONG)
-    BREWERY_ADDRESS_SECTION.appendChild(BREWERY_ADDRESS_CITY_PC)
+        const BREWERY_ADDRESS_CITY_PC = document.createElement('p')
+        const BREWERY_ADDRESS_CITY_PC_STRONG = document.createElement('strong')
+        BREWERY_ADDRESS_CITY_PC_STRONG.innerText = `${singleBrewery.city}, ${singleBrewery.postal_code}, ${singleBrewery.state}`
+        BREWERY_ADDRESS_CITY_PC.appendChild(BREWERY_ADDRESS_CITY_PC_STRONG)
+        BREWERY_ADDRESS_SECTION.appendChild(BREWERY_ADDRESS_CITY_PC)
 
-    const BREWERY_PHONE_SECTION = document.createElement('section')
-    BREWERY_PHONE_SECTION.setAttribute('class', 'phone')
+        const BREWERY_PHONE_SECTION = document.createElement('section')
+        BREWERY_PHONE_SECTION.setAttribute('class', 'phone')
 
-    const BREWERY_PHONE_HEADING_P = document.createElement('p')
-    BREWERY_PHONE_HEADING_P.innerText = 'Phone:'
-    BREWERY_PHONE_SECTION.appendChild(BREWERY_PHONE_HEADING_P)
+        const BREWERY_PHONE_HEADING_P = document.createElement('p')
+        BREWERY_PHONE_HEADING_P.innerText = 'Phone:'
+        BREWERY_PHONE_SECTION.appendChild(BREWERY_PHONE_HEADING_P)
 
-    const BREWERY_PHONE_NUMBER_P = document.createElement('p')
-    BREWERY_PHONE_NUMBER_P.innerText = singleBrewery.phone
-    BREWERY_PHONE_SECTION.appendChild(BREWERY_PHONE_NUMBER_P)
+        const BREWERY_PHONE_NUMBER_P = document.createElement('p')
+        BREWERY_PHONE_NUMBER_P.innerText = singleBrewery.phone
+        BREWERY_PHONE_SECTION.appendChild(BREWERY_PHONE_NUMBER_P)
 
-    const BREWERY_URL_SECTION = document.createElement('section')
-    BREWERY_URL_SECTION.setAttribute('class', 'link')
+        const BREWERY_URL_SECTION = document.createElement('section')
+        BREWERY_URL_SECTION.setAttribute('class', 'link')
 
-    const BREWERY_URL_LINK = document.createElement('a')
-    BREWERY_URL_LINK.setAttribute('href', singleBrewery.website_url)
-    BREWERY_URL_LINK.setAttribute('target', '_blank')
-    BREWERY_URL_LINK.innerText = 'Visit Website'
+        const BREWERY_URL_LINK = document.createElement('a')
+        BREWERY_URL_LINK.setAttribute('href', singleBrewery.website_url)
+        BREWERY_URL_LINK.setAttribute('target', '_blank')
+        BREWERY_URL_LINK.innerText = 'Visit Website'
 
-    BREWERY_URL_SECTION.appendChild(BREWERY_URL_LINK)
+        BREWERY_URL_SECTION.appendChild(BREWERY_URL_LINK)
 
+        BREWERY_LI.appendChild(BREWERY_H2)
+        BREWERY_LI.appendChild(BREWERY_TYPE_DIV)
+        BREWERY_LI.appendChild(BREWERY_ADDRESS_SECTION)
+        BREWERY_LI.appendChild(BREWERY_PHONE_SECTION)
+        BREWERY_LI.appendChild(BREWERY_URL_SECTION)
 
-    BREWERY_LI.appendChild(BREWERY_H2)
-    BREWERY_LI.appendChild(BREWERY_TYPE_DIV)
-    BREWERY_LI.appendChild(BREWERY_ADDRESS_SECTION)
-    BREWERY_LI.appendChild(BREWERY_PHONE_SECTION)
-    BREWERY_LI.appendChild(BREWERY_URL_SECTION)
-
-    BREWERY_LIST.appendChild(BREWERY_LI)
+        BREWERY_LIST.appendChild(BREWERY_LI)
+    }
 }
 
 function setup() {
