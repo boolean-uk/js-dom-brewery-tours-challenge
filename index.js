b42bc6603feb88c4ca8d89b833c6ba9cd6b74a9b
MattVwaves <jamesmattholt@gmail.com>
2022-10-28 17:18:00 +0100

    extra brew info and filters added

--- index.js
+++ index.js
@@ -2,13 +2,21 @@
 state = {
     breweries:[],
 // add filters to state
-    filterByBrewType: ['micro', 'brewpub', 'regional']
+    filterByBrewType: ['micro', 'brewpub', 'regional'],
+    filterMicro: 'micro',
+    filterBrewpub: 'brewpub',
+    filterRegional: 'regional'
 }
 // query select-state & submit & breweries-list
 const stateInput = document.getElementsByTagName("input")[0]
 const submit = document.getElementsByTagName("input")[1]
 const breweryUl = document.querySelector('.breweries-list')
-
+// target select options
+const select = document.getElementsByTagName("select")[0]
+// select eventlistener
+select.addEventListener('change', ()=>{
+    return select.value
+})
 // add eventlistener on submit 
 submit.addEventListener('click', (event)=>{
     event.preventDefault()
@@ -28,15 +36,16 @@ submit.addEventListener('click', (event)=>{
     })
     }
 )
+
 function renderStateSearch (){
     breweryUl.innerHTML = ''
-// implement type filter
+// implement initial type filter
     let typeFilteredList = state.breweries.filter((brew)=> {
         if (state.filterByBrewType.includes(brew.brewery_type)){
             return brew
         }
     })
-// render filtered list
+// create filtered state brewery list
     typeFilteredList.forEach((brew)=> {
 
         const brewCard = document.createElement('li')
@@ -65,6 +74,81 @@ function renderStateSearch (){
         city.setAttribute('style', 'font-weight: bold')
         city.innerText = `${brew.city}, ${brew.postal_code}`
         addSec.appendChild(city)
+
+        const phoneSec = document.createElement('section')
+        brewCard.appendChild(phoneSec)
+        const phone = document.createElement('h3')
+        phone.innerText = 'Phone'
+        phoneSec.appendChild(phone)
+        const phoneNum = document.createElement('p')
+        phoneNum.innerText = brew.phone
+        phoneSec.appendChild(phoneNum)
+
+        const linkSec = document.createElement('section')
+        linkSec.setAttribute('class', 'link')
+        brewCard.appendChild(linkSec)
+        const link = document.createElement('a')
+        link.innerText = 'Visit Website'
+        link.href = brew.website_url
+        linkSec.appendChild(link)
     })
 }
+// 3 extra filters 
+select.addEventListener('change', ()=>{
+    breweryUl.innerHTML = ''
+
+    let typeFilteredList2 = state.breweries.filter((brew)=>{
+        if (select.value === brew.brewery_type)
+        return brew
+    })
+    console.log(typeFilteredList2)
+    typeFilteredList2.forEach((brew)=> {
+
+    
+        const brewCard = document.createElement('li')
+        breweryUl.appendChild(brewCard)
+
+        const brewName = document.createElement('h2')
+        console.log(brew)
+        brewName.innerText = brew.name
+        brewCard.appendChild(brewName)
+
+        const brewType = document.createElement('div')
+        brewType.setAttribute('class', 'type')
+        brewType.innerText = brew.brewery_type
+        brewCard.appendChild(brewType)
+
+        const addSec = document.createElement('section')
+        addSec.setAttribute('class', 'address')
+        brewCard.appendChild(addSec)
+        const add = document.createElement('h3')
+        add.innerText = 'Address'
+        addSec.appendChild(add)
+        const street = document.createElement('p')
+        street.innerText = brew.street
+        addSec.appendChild(street)
+        const city = document.createElement('p')
+        city.setAttribute('style', 'font-weight: bold')
+        city.innerText = `${brew.city}, ${brew.postal_code}`
+        addSec.appendChild(city)
+
+        const phoneSec = document.createElement('section')
+        brewCard.appendChild(phoneSec)
+        const phone = document.createElement('h3')
+        phone.innerText = 'Phone'
+        phoneSec.appendChild(phone)
+        const phoneNum = document.createElement('p')
+        phoneNum.innerText = brew.phone
+        phoneSec.appendChild(phoneNum)
+
+        const linkSec = document.createElement('section')
+        linkSec.setAttribute('class', 'link')
+        brewCard.appendChild(linkSec)
+        const link = document.createElement('a')
+        link.innerText = 'Visit Website'
+        link.href = brew.website_url
+        linkSec.appendChild(link)
+    })
+    
+})
 
