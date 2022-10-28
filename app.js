f99def408ddfebeb5d82f51d3c1f910c6f304daa
George Roth <g@georgeroth.eu>
2022-10-28 19:29:28 +0100

    Initial planning done

---
+++ app.js
@@ -0,0 +1,28 @@
+// ACCEPTANCE CRITERIA:
+// 1. Connect to Open Brewery DB using Insomnia to figure out how to get the data
+// 2. Add an event listener to the search button that:
+// 3.  – Calls a function that imports relevant brewery data into local state based on state entered into search
+// 4.  – And removes everything other than "Micro", "Regional" or "Brewpub"
+// 5.  – Calls another function that renders the page from the local state as found in "standard-list-items.html"
+// 6. Add an event listener to the filter dropdown that:
+// 7.  – Calls a function that filters the state into another variable accordingly
+// 8.  – Calls the render function
+// 
+// EXTENSION #1:
+// 1. Add search section to index.html
+// 2. Add event listener to each keyboard stroke in the input field that:
+// 3.  – Calls a function that filters the state into another variable accordingly
+// 4.  – Calls the render function
+//
+// EXTENSION #2:
+// 1. Add cities outline into index.html
+// 2. For each city, create a new tickbox in JS with its own event listener that: 
+// 3.  – Calls a function that filters the state into another variable accordingly
+// 4.  – Calls the render function
+// 5. Add clear all button that re-runs the for each in point #2 (hopefully this will work)
+//
+// EXTENSION #3:
+// 1. No idea about the pagination yet. Maybe filter and display breweries by index?
+//
+// EXTENSION #4:
+// 1. If I ever get here, do something similar to the to-do-list...?
