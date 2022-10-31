23db674f76dac6c6b5abd7169f97f19af24c54f5
Dave <Daverevell491@gmail.com>
2022-10-31 12:13:06 +0000

    card rendering started

--- index.js
+++ index.js
@@ -1,44 +1,93 @@
 // targeting
-// target .breweries-list
-
+const breweriesList = document.querySelector('.breweries-list')
 
 // Local state
-
+const state = {
+  types: ['regional', 'micro', 'brewpub'],
+  breweries: [],
+  filterByType: ''
+}
 
 // RENDERING
 
-// Breweries function
-  // clear html
-  // for each brewery create a card (point to card rendering function)
-// function end
+// function to trigger all rendering needed
+function render() {
+  renderBreweries()
 
+  // call Ext. 2 render function
+}
+
+// Breweries function
+function renderBreweries() {
+  // clear html
+  breweriesList.innerHTML = ''
+
+  // apply filters function
+  const breweries = applyFilters()
+  // for each brewery create a card (point to card rendering function)
+  breweries.forEach(renderBreweryCard)
+  // function end
+}
 
 // Filters function
-  // create cities array
-  // search through the array for the requested cities/state
-  // make sure it only finds micro, regional or brewpub breweries
-// function end
+function applyFilters() {
+  // variable for state.breweries
+  let filteredBreweries = state.breweries
 
+  // filter breweries by type
+  if(state.filterByType !== null) {
+    filteredBreweries = filteredBreweries.filter(brewery => brewery.type === state.filterByType)
+  }
+
+  // Ext. 1 - filter breweries by name
+
+  // Ext. 2 - filter breweries by city
+
+  return filteredBreweries
+  // function end
+}
 
 // Card rendering function
+function renderBreweryCard(brewery) {
   // create li
+  const li = document.createElement('li')
+  li.innerText = 'hello'
   // create h2 for title
+  const h2 = document.createElement('h2')
   // create div .type
-  // create section for address
-  // h3 for address title
-  // p for address line 1
-  // p for address line 2 /w strong
-  // create section for phone number
-  // h3 for phone title
-  // p for number
-  // section for website .link
-  // anchor link to brewery site
+  const div = document.createElement('div')
+  div.setAttribute('class', 'address')
 
-  // apply inner text based on the state data
+  // create section for address
+  const sectionAddress = document.createElement('section')
+  // h3 for address title
+  const h3Address = document.createElement('h3')
+  // p for address line 1
+  const pAddressL1 = document.createElement('p')
+  // p for address line 2 /w strong
+  const pAddressL2 = document.createElement('p')
+  sectionAddress.appendChild(h3Address, pAddressL1, pAddressL2)
+
+  // create section for phone number
+  const sectionNumber = document.createElement('section')
+  // h3 for phone title
+  const h3Phone = document.createElement('h3')
+  // p for number
+  const pPhone = document.createElement('p')
+  sectionNumber.appendChild(h3, pPhone)
+
+  // section for website .link
+  const sectionSite = document.createElement('section')
+  // anchor link to brewery site
+  const a = document.createElement('a')
+  sectionSite.appendChild(a)
 
   // append everything to where they should be
-// function end
+  breweriesList.appendChild(li)
+  li.appendChild(h2, div, sectionAddress, sectionNumber, sectionSite)
 
+  // function end
+}
 
 // EVENT LISTENERS
 
@@ -57,7 +106,20 @@
   // update city filter within state
 // EL end
 
+
 // Ext. 1
+// function to search by name
 
 
 // Ext. 2
+
+// cityFilter function
+  // create cities array
+  // search through the array for the requested cities
+  // make sure it only finds micro, regional or brewpub breweries
+// function end
+
+// Ext. 3
+
+
+//Ext. 4
