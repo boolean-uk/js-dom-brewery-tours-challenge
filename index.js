f10a7f5ce53787c77d2b84f195dfd9b863fed250
Kat <katdemey@outlook.com>
2023-01-10 16:06:40 +0000

    hardcoded ohio to check code works overall bofre replacing it with more flexible code as a form of pre-debugging. first part of core requirement almost done. Now need to filter out so that only the brewery types we want will show. Then flexible coding can be done

--- index.js
+++ index.js
@@ -11,43 +11,114 @@ const brewriesUL = document.querySelector("#breweries-list")
 const searchForm = document.querySelector("#select-state-form")
 const searchState = document.querySelector("#select-state")
 
-searchForm.addEventListener("click", (event) => {
-    event.preventDefault
+//variables I want to reach in multiple places: scopeability?
+// const searchForState = searchState.value
+const stateName = "ohio"
 
-    const searchForState = searchState.value
-    if (searchForState > 0) {
+//EVENT LISTENER: SEARCH
+searchForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    // const stateName
+
+    if (stateName.length > 0) {
         //call function that will call the brewries by that state
         getBrewries(stateName)
-    } else {
-        alert("Please type in the state you are visiting first then try again")
     }
+    //else {
+    //     alert("Please type in the state you are visiting first then try again")
+    // }
 })
+
 
 //NETWORK REQUESTS
 function getBrewries(stateName) {
-    // capitalise input (so if user types "ohio" we get "Ohio" so that it matches brewries.name )
+
     // if "" -> "_"
 
-    fetch("API") // send a Request *******
+    //GET by name
+    fetch(`https://api.openbrewerydb.org/breweries?by_state=${stateName}&per_page=50`
+    ) // send a Request *******
         .then((response) => {
             // response = the Response from the server
             return response.json();
         })
         .then((responseData) => {
-            // responseData = response.json()
-
             // we have received all todo items
             console.log("Received brewries list", responseData);
+
+            //then filter so you only get micro, brewpub and regional
+            const filteredBrewries = state.brewries.filter((responseData) =>
+            responseData.brewery_type !== breweryInfo.brewery_type
+            );
             // update local STATE with fetched people
             state.brewries = responseData;
+
             // render each brewry
-            renderBrewry(); //renderBrewries
+            renderBrewry(); //renderBrewries after the filter
         });
+    // state.brewries = filteredBrewries
 }
 
 
 //RENDERING
+//only rendering the first brewery atm
 function renderBrewry() {
+    // const breweryInfo = state.brewries
+
+    //for Each? (inputvalue === "state")
+    state.brewries.forEach((brewery) => {
+        //create elements
+        const li = document.createElement("li")
+        brewriesUL.append(li)
+
+        const h2 = document.createElement("h2")
+        h2.innerText = brewery.name //"name"
+        li.append(h2)
+
+        const div = document.createElement("div")
+        div.setAttribute("class", "type")
+        div.innerText = brewery.brewery_type //"brewery_type"
+        li.append(div)
+
+        const section = document.createElement("section")
+        section.setAttribute("class", "address")
+        li.append(section)
+
+        const h3 = document.createElement("h3")
+        h3.innerText = "Address:"
+        section.append(h3)
+
+        const p = document.createElement("p")
+        p.innerText = brewery.street //"street"": <p>9511 Kile Rd</p>
+        section.append(p)
+
+        const p2 = document.createElement("p")
+        // p2.setAttribute = strong //style it..check (need to create another element )
+        p2.innerHTML =`<strong> ${brewery.city} + ${brewery.postal_code}</strong>` // brewery.city + brewery.postal_code //"postal_code" e.g Chardon, 44024 check syntax
+        section.append(p2)
+
+        const sectionPhone = document.createElement("section")
+        sectionPhone.setAttribute("class", "phone")
+        li.append(sectionPhone)
+
+        const h3Phone = document.createElement("h3")
+        h3Phone.innerText = "Phone:"
+        sectionPhone.append(h3Phone)
+
+        const pPhone = document.createElement("p")
+        pPhone.innerText = brewery.phone //(object key:) "phone"
+        sectionPhone.append(pPhone)
+
+        const sectionLinkToBrewry = document.createElement("section")
+        sectionLinkToBrewry.setAttribute("class", "link")
+        li.append(sectionLinkToBrewry)
+
+        const a = document.createElement("a")
+        a.innerText = "Visit Website"
+        a.setAttribute("href", brewery.website_url) //${breweryInfo.website_url} update null to "website_url"
+        a.setAttribute("target", "_blank")
+        sectionLinkToBrewry.append(a)
+    })
 
 }
 
