07aa6a0780af23a1d2ca80ff6bdb33bfb678b1a8
Oliver West <oliversimon.west@gmail.com>
2023-01-11 18:20:59 +0000

    Cleaned up code, and called functions with other functions as parameters where possible - also added comments throughout code for easy explanation

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const state = {
 };
 
 // VARIABLES
+// Used by the initial state search form listener 
+// to make sure the state is spelled correctly.
 const usStates = [
     "alabama", "alaska", "arizona", "arkansas", 
     "california", "colorado", "connecticut", "delaware", 
@@ -19,6 +21,9 @@ const usStates = [
     "vermont", "virginia", "washington", "west virginia", 
     "wisconsin"
 ]
+
+// Used for the filter by cities.
+let checkedCities = []
 
 // SELECT EXISTING HTML ELEMENTS
 const stateForm = document.querySelector("#select-state-form")
@@ -39,40 +44,47 @@ function submitFormListen() {
         // stateInput.value = what was typed in.
         const stateSubmit = stateInput.value.toLowerCase()
         if(usStates.find(stateName => stateName === stateSubmit) === undefined){
-            // must be a state, and spelt correctly.
+            // must be a word that is in the array of US States above, and spelt correctly.
             return alert("That is not a real/correctly spelled State")
         }
-        // regex to replace all spaces with an underscore
+        // regex to replace all spaces with an underscore - for fetch URL use.
         const true_state = (stateSubmit.replace(/ /g,"_"))
-
+        // Fetch the Data from the API using the state.
         getAllBreweries(true_state)
     })
 }
 
 function filterTypeListen() {
+    // Listening for the Type of Brewery drop down.
     filterForm.addEventListener('change', (event) => {
         const selectInput = event.target.value
-        const filterType = filterBreweriesByType(selectInput)
-        renderBreweries(filterType)
-        renderCities(filterType)
+        // Passing the FILTER BY TYPE function directly into the
+        // render breweries and cities.
+        renderBreweries(filterBreweriesByType(selectInput))
+        renderCities(filterBreweriesByType(selectInput))
     })
 }
 
 function searchBarListen() {
+    // Listening for the Search Bar - each stroke.
     searchForm.addEventListener("input", (event) => {
         const searchQuery = event.target.value
-        const filterType = filterBreweriesByName(searchQuery)
-        renderBreweries(filterType)
-        renderCities(filterType)
+        // Passing the FILTER BY NAME function directly into the
+        // render breweries and cities.
+        renderBreweries(filterBreweriesByName(searchQuery))
+        renderCities(filterBreweriesByName(searchQuery))
     })
 }
 
 function clearAllListen() {
+    // Listening for the clear all button.
     clearButton.addEventListener("click", () => {
+        // Empty the checkedCities array so no duplicates.
         checkedCities = []
-        const filterType = filterBreweriesAll()
-        renderBreweries(filterType)
-        renderCities(filterType)
+        // Passing the ALL filter - so all micros/regionals/brewpubs 
+        // get rendered by breweries/cities.
+        renderBreweries(filterBreweriesAll())
+        renderCities(filterBreweriesAll())
         
     })
 }
@@ -84,17 +96,21 @@ function getAllBreweries(stateName) {
         return response.json()
     })
     .then((allBreweries) => {
+        // setting state.breweries to the data from the API
         state.breweries = allBreweries
-        const filterType = filterBreweriesAll()
-        renderBreweries(filterType)
-        renderCities(filterType)
+        // Passing the filtered breweries (micro/regional/brewpub) directly into the
+        // renderBreweries and renderCities functions.
+        renderBreweries(filterBreweriesAll())
+        renderCities(filterBreweriesAll())
     })
 
 }
 // FILTER FUNCTIONS
 
 function filterBreweriesByType(type) {
+    // the type being passed is either micro, regional, or brewpub.
     const filteredBreweries = state.breweries.filter((data) => {
+        // all breweries that match the type, are kept in the array.
         if(data.brewery_type === type) {
             return true
         }
@@ -103,16 +119,20 @@ function filterBreweriesByType(type) {
 }
 
 function filterBreweriesAll() {
+    // filters the FETCH data and only keeps brewery_types of micro, regional, or brewpub.
     const filteredBreweries = state.breweries.filter((data) => {
         if(data.brewery_type === "micro" || data.brewery_type === "regional" || data.brewery_type === "brewpub") {
             return true
         }
     })
+    // sets our state to the filtered array.
     state.breweries = filteredBreweries
     return filteredBreweries
 }
 
 function filterBreweriesByName(letters) {
+    // if the name of the brewery contains any of the letters passed to this
+    // function, then it is kept and returned.
     const filteredBreweries = state.breweries.filter((data) => {
         const breweryName = data.name.toLowerCase()
         if(breweryName.includes(letters)) {
@@ -123,9 +143,8 @@ function filterBreweriesByName(letters) {
 }
 
 function filterByCity(city) {
+    // if the city matches the state.breweries.city, then it is kept and returned.
     const filteredBreweries = state.breweries.filter((data) => {
-        // const cityName = city.toLowerCase()
-        console.log("data:", data.city, "city name:", city)
         if(city === data.city) {
             return true
         }
@@ -200,15 +219,17 @@ function renderBreweries(breweries) {
     
   }
 
-let checkedCities = []
 function renderCities(breweries){
     cityForm.innerHTML = "";
     
+    // Mapping over the filtered array of breweries, creating an array of cities.
     const cities = breweries.map((company) => company.city)
+    // Filter through that array and remove duplicates.
     const filteredCities = cities.filter((city, index) => cities.indexOf(city) === index).sort()
     
-
+    // Render each city from the filtered array.
     filteredCities.forEach((city) => {
+        // CREATING
         const cityInput = document.createElement("input")
         cityInput.type = "checkbox"
         cityInput.name = city
@@ -218,25 +239,32 @@ function renderCities(breweries){
         cityLabel.for = city
         cityLabel.innerText = city
 
+        // Listening for check marks on cities, to then filter and re-render breweries.
         cityInput.addEventListener("change", () => {
             if(cityInput.checked) {
+                // If checked, push the city into the checked cities array, 
+                // and rerender breweries from that array.
                 checkedCities.push(city)
                 const filteredBreweries = state.breweries.filter((brewery) => checkedCities.includes(brewery.city))
                 renderBreweries(filteredBreweries)
             } else {
+                // If unchecked - and checkedCities array has more than 1 city
+                // remove that city from the array and re-render the breweries.
                 if(checkedCities.length > 1){
                     checkedCities = checkedCities.filter((item) => item !== city)
                     const filteredBreweries = state.breweries.filter((brewery) => checkedCities.includes(brewery.city))
                     renderBreweries(filteredBreweries)
                 } else if(checkedCities.length === 1) {
+                    // If unchecked - and checkedCities array has only 1 city in it.
+                    // set checkedCities to empty, and rerender all cities/breweries using
+                    // the all filter, to get back all breweries.
                     checkedCities = []
-                    const filterType = filterBreweriesAll()
-                    renderBreweries(filterType)
-                    renderCities(filterType)
+                    renderBreweries(filterBreweriesAll())
+                    renderCities(filterBreweriesAll())
                 }
             }
         })
-        
+        // APPENDING
         cityForm.append(cityInput, cityLabel)
     })
 }
