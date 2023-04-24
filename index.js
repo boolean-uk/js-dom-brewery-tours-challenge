91401710baa214b125b9ab714f4951beceecaf27
Alex Shaw <alexjshaw@gmail.com>
2023-04-24 15:23:19 +0100

    breweries are displayed after search. Websites work. If no address is found it replaces with No Given Address. If no address 2 is found it hides it from the page.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const state = {
 // Write search term to variable
 
 const searchField = document.querySelector(`#select-state-form`)
-
+const breweryList = document.querySelector(`#breweries-list`)
 
 // EXTRACTS SEARCH TERM, FETCHES DATA USING THAT TERM, CHECKS BREWERY TYPE AND WRITES VALID
 // BREWERIES TO STATE AND ADDS VISIBLE PROPERTY
@@ -26,8 +26,54 @@ function searchSubmit() {
                 return false
             }
         })
-    })
+    renderCards()
     return state.breweries
+    })
+}
+// RENDER BREWERY CARDS
+function renderCards() {
+    breweryList.innerHTML = ``
+    state.breweries.forEach(brewery => {
+        const li = document.createElement(`li`)
+        if (brewery.address_1 === null) {
+            brewery.address_1 = `No Given Address`
+        }
+        if (brewery.address_2 === null) {
+        li.innerHTML = `
+        <h2>${brewery.name}</h2>
+        <div class="type">${brewery.brewery_type}</div>
+        <section class="address">
+          <h3>Address:</h3>
+          <p>${brewery.address_1}</p>
+        </section>
+        <section class="phone">
+          <h3>Phone:</h3>
+          <p>${brewery.phone}</p>
+        </section>
+        <section class="link">
+          <a href="${brewery.website_url}" target="_blank">Visit Website</a>
+        </section>
+        `
+    } else {
+        li.innerHTML = `
+        <h2>${brewery.name}</h2>
+        <div class="type">${brewery.brewery_type}</div>
+        <section class="address">
+          <h3>Address:</h3>
+          <p>${brewery.address_1}</p>
+          <p><strong>${brewery.address_2}</strong></p>
+        </section>
+        <section class="phone">
+          <h3>Phone:</h3>
+          <p>${brewery.phone}</p>
+        </section>
+        <section class="link">
+          <a href="${brewery.website_url}" target="_blank">Visit Website</a>
+        </section>
+        `
+    }
+        breweryList.append(li)
+    })
 }
 
 function print() {
@@ -42,7 +88,7 @@ function searchEventListener() {
     searchField.addEventListener('submit', (event) => {
         event.preventDefault()
         searchSubmit()
-        print ()
+        // print ()
     })
 }
 
