3f4f2e26d752dac023001c5b406084ff2fad61b6
Isabelle <isabel.tartarelli@live.it>
2023-04-24 13:45:42 +0100

    created array for data and stopped page from loading

---
+++ index.js
@@ -0,0 +1,13 @@
+// Array for data
+const breweries = {
+  breweryList: []
+};
+
+// Stop page from loading & working input
+const form = document.querySelector("form")
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+
+  const input = document.querySelector("#select-state");
+
+} )
