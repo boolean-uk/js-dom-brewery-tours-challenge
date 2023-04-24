380fd1f2ab776eff5c59a56c1d1009bacf46b99a
Isabelle <isabel.tartarelli@live.it>
2023-04-24 14:20:09 +0100

    Fetch function working and correctly displaiyng the data if console logged. Created api variable

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // Array for data
 const breweries = {
-  breweryList: []
+  breweryList: [],
 };
 
+const api = "https://api.openbrewerydb.org/v1/breweries?by_state=";
+
+// GET
+function getData() {
+  fetch(api)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      breweries.breweryList = data;
+      console.log("data", data);
+    });
+}
+
+getData();
 // Stop page from loading & working input
-const form = document.querySelector("form")
+const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
-  event.preventDefault()
+  event.preventDefault();
 
   const input = document.querySelector("#select-state");
-
-} )
+  const values = input.value;
+  console.log("input value:", values);
+  form.reset();
+});
