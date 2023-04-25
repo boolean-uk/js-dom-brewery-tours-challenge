697623a7a65c67a0b0d345eb346be29fffb1138e
Alex Shaw <alexjshaw@gmail.com>
2023-04-25 15:07:35 +0100

    extension 2 completed

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const state = {
     breweries: [],
-    searchedCities: []
+    citySearchList: [],
+    citySearchActive: [],
 }
 
 // FILE WIDE CONSTANTS
 
 const searchField = document.querySelector(`#select-state-form`)
 const breweryList = document.querySelector(`#breweries-list`)
-
+const cityFilterForm = document.querySelector(`#filter-by-city-form`)
 
 // EXTRACTS SEARCH TERM, FETCHES DATA USING THAT TERM, CHECKS BREWERY TYPE AND WRITES VALID
 // BREWERIES TO STATE AND ADDS VISIBLE PROPERTY
 function searchSubmit() {
     const searchText = searchField[`select-state`].value
+    if (searchText === ``) {
+        alert(`Search Cannot Be Empty - Please Enter a State`)
+        return
+    }
     fetch(`https://api.openbrewerydb.org/v1/breweries?by_state=${searchText}`)
     .then(function (response) {
         return response.json()
@@ -21,19 +26,71 @@ function searchSubmit() {
         state.breweries = []
         data.filter(currentBrewery => {
             if (currentBrewery.brewery_type === `micro` || currentBrewery.brewery_type === `regional` || currentBrewery.brewery_type === `brewpub`) {          
-                state.breweries.push({...currentBrewery, filter: true, search: true})
+                state.breweries.push({...currentBrewery, filter: true, search: true, cityFilter: true})
             } 
         })
+        if (state.breweries.length === 0) {
+            alert(`No Results Found`)
+            return
+        }
     renderCards(state.breweries)
+    createCityArray(state.breweries)
     })
+}
+
+// CREATES AN ARRAY OF EVERY CITY FROM THE CURRENTLY SELECTED STATE
+function createCityArray(arr) {
+    state.citySearchList = []
+    arr.forEach(brewery => {
+        if (state.citySearchList.length === 0 || !state.citySearchList.includes(`${brewery.city}`)) {
+        state.citySearchList.push(brewery.city)
+        }
+    })
+    renderCitySearchButtons(state.citySearchList)
+}
+
+// CREATES A CHECK BOX FOR EVERY CITY
+function renderCitySearchButtons(arr) {
+    cityFilterForm.innerHTML = ``
+    arr.forEach(city => {
+        const cityInput = document.createElement(`input`)
+            cityInput.setAttribute(`type`, `checkbox`)
+            cityInput.setAttribute(`name`, `${city}`)
+            cityInput.setAttribute(`value`, `${city}`)
+        const cityLabel = document.createElement(`label`)
+            cityLabel.setAttribute(`for`, `${city}`)
+            cityLabel.innerText = `${city}`
+        cityFilterForm.append(cityInput)
+        cityFilterForm.append(cityLabel)
+        cityInput.addEventListener(`click`, () => {
+            cityFilterArray(cityInput, cityInput.value)
+        })
+    })
+}
+
+// CREATES ARRAY OF ACTIVE FILTERS AND MODIFIES STATE.BREWERIES TO SHOW ACTIVE ONLY
+function cityFilterArray(cityInput, city) {
+    if (cityInput.checked) {
+        state.citySearchActive.push(city)
+    } else {
+        state.citySearchActive.splice(state.citySearchActive.indexOf(city), 1)
+    }
+    state.breweries.forEach(brewery => {
+        const pattern = new RegExp(brewery.city)
+        if (pattern.test(state.citySearchActive)) {
+            brewery.cityFilter = true
+        } else {
+            brewery.cityFilter = false
+        }
+    })
+    renderCards(state.breweries)
 }
 
 // RENDER BREWERY CARDS
 function renderCards(arr) {
     breweryList.innerHTML = ``
     arr.forEach(brewery => {
-        if (brewery.filter === true && brewery.search === true) {
-        
+        if (brewery.filter === true && brewery.search === true && brewery.cityFilter === true) {
         const li = document.createElement(`li`)
         li.innerHTML = `
         <h2>${brewery.name}</h2>
@@ -77,6 +134,7 @@ function filterArray(filter) {
     return state.breweries
 }
 
+// ADDS EVENT LISTENER TO LIVE SEARCH BAR
 function brewerySearchEventListener() {
     const searchArea = document.querySelector(`#search-breweries-form`)
     searchArea.addEventListener(`keyup`, (event) => {
@@ -87,6 +145,7 @@ function brewerySearchEventListener() {
     })
 }
 
+// UPDATES STATE BASED ON SEARCH TEXT
 function brewerySearchArray(searchTerm) {
     const pattern = new RegExp(searchTerm, `i`)
     state.breweries.forEach(brewery => {
@@ -99,13 +158,11 @@ function brewerySearchArray(searchTerm) {
     renderCards(state.breweries)
 }
 
-// Changes visible value based on other events
-
 // SEARCH BUTTON - PREVENTS PAGE REFRESH AND FUNS SEARCHSUBMIT FUNCTION
 function searchEventListener() {
     searchField.addEventListener('submit', (event) => {
         event.preventDefault()
-        const searchArea = document.querySelector(`#brewery-search`)
+        const searchArea = document.querySelector(`#search-breweries`)
         searchArea.value = ``
         searchSubmit()
     })
