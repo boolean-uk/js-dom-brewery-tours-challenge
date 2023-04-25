5500549a2391e3985a9450d12d7aff523c0d5282
Todd Griffin <todd.griffin61@outlook.com>
2023-04-25 09:24:37 +0100

    used comments, made code more readable

--- index.js
+++ index.js
@@ -1,61 +1,62 @@
 const state = {}
 
 const searchForm = document.querySelector('#select-state-form')
+const breweryUl = document.querySelector('#breweries-list')
 
+// Event listener for submit button
 searchForm.addEventListener('submit', (event) => {
   event.preventDefault()
   const userInput = document.querySelector('#select-state').value
   console.log('user input:', userInput)
   state.userInput = userInput
 
-  let filter = 'Micro'
   const filterDropdown = document.querySelector('#filter-by-type')
-  console.log('dropdown: ', filterDropdown)
+  // console.log('dropdown: ', filterDropdown)
   state.filter = filterDropdown.value
-  console.log('filter:', state.filter)
+  // console.log('filter:', state.filter)
+
   getBreweriesByState(state.userInput)
 })
 
-const breweries = [
-  {
-    address_2: null,
-    address_3: null,
-    brewery_type: "large",
-    city: "San Diego",
-    country: "United States",
-    county_province: null,
-    created_at: "2018-07-24T00:00:00.000Z",
-    id: 8041,
-    latitude: "32.714813",
-    longitude: "-117.129593",
-    name: "10 Barrel Brewing Co",
-    obdb_id: "10-barrel-brewing-co-san-diego",
-    phone: "6195782311",
-    postal_code: "92101-6618",
-    state: "California",
-    street: "1501 E St",
-    updated_at: "2018-08-23T00:00:00.000Z",
-    website_url: "http://10barrel.com"
-  }
-];
-
+// GET request based on user input
 function getBreweriesByState(state) {
-  let queryResult 
   fetch(`https://api.openbrewerydb.org/v1/breweries?by_state=${state}`)
-  .then(res => queryResult = res.json())
+  .then(res => res.json())
   .then((json) => {
       addBreweriesToState(json)
-      console.log('response:', json)
+      // console.log('response:', json)
       renderBreweries()
     })
 }
 
+// For some reason adding this line directly to getBreweriesByState() causes an error
 function addBreweriesToState(arrOfBreweries) {
   state.breweries = arrOfBreweries
 }
 
-const breweryUl = document.querySelector('#breweries-list')
+// Filter logic for rendering the list
+function filterMatchesType(stateFilter, breweryType) {
+  if (stateFilter === '') {
+    return true
+  }
+  else if (stateFilter === breweryType) {
+    return true
+  }
+  return false
+}
 
+// Rendering the entire list
+function renderBreweries() {
+  breweryUl.innerHTML = ''
+  for (let i = 0; i < state.breweries.length; i++) {
+    // console.log(`state.filter: ${state.filter}\nBrewery type: ${state.breweries[i].brewery_type}`)
+    // console.log(`filters match: ${filterMatchesType(state.filter, state.breweries[i].brewery_type)}`)
+    if (!(filterMatchesType(state.filter, state.breweries[i].brewery_type))) continue
+    breweryUl.append(createBreweryLi(state.breweries[i]))
+  }
+}
+
+// Create the brewery cards
 function createBreweryLi(breweryObj) {
   const li = document.createElement('li')
 
@@ -99,31 +100,3 @@ function createBreweryLi(breweryObj) {
 
   return li
 }
-
-function filterMatchesType(stateFilter, breweryType) {
-  if (stateFilter === '') {
-    return true
-  }
-  else if (stateFilter === breweryType) {
-    return true
-  }
-  return false
-}
-
-function renderBreweries() {
-  breweryUl.innerHTML = ''
-  // console.log('rendering with filter', state.filter)
-
-  state.filter
-
-  for (let i = 0; i < state.breweries.length; i++) {
-    console.log(`state.filter: ${state.filter}\nBrewery type: ${state.breweries[i].brewery_type}`)
-    console.log(`filters match: ${filterMatchesType(state.filter, state.breweries[i].brewery_type)}`)
-    if (!(filterMatchesType(state.filter, state.breweries[i].brewery_type))) continue
-    breweryUl.append(createBreweryLi(state.breweries[i]))
-  }
-}
-
-
-// getBreweriesByState('California')
-
