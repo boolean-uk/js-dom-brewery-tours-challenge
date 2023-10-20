db5029b749fa45062efe4ea0490f0d321ef7d27b
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-10-20 12:45:02 +0100

    fetch function refined

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const STATE = {};
+const STATE = {
+  breweries: {
+    micro: [],
+    regional: [],
+    brewpub: [],
+  },
+};
 const SELECT_STATE_FORM = document.querySelector("#select-state-form");
 const SELECT_STATE_INPUT = SELECT_STATE_FORM.querySelector(
   ":scope > #select-state"
@@ -9,11 +15,30 @@ function init() {
 }
 
 function getBreweries(filter) {
+  const breweryTypes = Object.keys(STATE.breweries);
+
+  return Promise.all(
+    breweryTypes.map((type) => {
+      return getBreweryType(type, filter, 1);
+    })
+  );
+}
+
+function getBreweryType(type, filter, page) {
+  page = page ? page : 1;
+  console.log('page', page)
   return new Promise((resolve, reject) =>
-    fetch(`https://api.openbrewerydb.org/v1/breweries${filter ? filter : ""}`)
+    fetch(
+      `https://api.openbrewerydb.org/v1/breweries?per_page=200&page=${page}&by_type=${type}${
+        filter ? filter : ""
+      }`
+    )
       .then((res) => res.json())
       .then((res) => {
-        STATE.breweries = res;
+        STATE.breweries[type].push(...res);
+        if (res.length === 200) {
+          getBreweryType(type, filter, ++page);
+        }
         resolve();
       })
       .catch((error) => {
@@ -23,15 +48,15 @@ function getBreweries(filter) {
 }
 
 function getBreweriesByState(state) {
-  const filter = `?by_state=${washInput(state)}`;
+  const filter = `&by_state=${washInput(state)}`;
   getBreweries(filter).then(() => renderBreweries());
 }
 
 function usStateInput() {
   SELECT_STATE_FORM.addEventListener("submit", (e) => {
     e.preventDefault();
-    
-    getBreweriesByState(SELECT_STATE_INPUT.value)
+
+    getBreweriesByState(SELECT_STATE_INPUT.value);
 
     SELECT_STATE_FORM.reset();
   });
@@ -42,7 +67,9 @@ function washInput(input) {
 }
 
 function renderBreweries() {
-  console.log('STATE', STATE)
+  console.log("STATE", STATE);
 }
 
-init()
+init();
+
+getBreweriesByState("new_york");
