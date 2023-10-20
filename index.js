87b7125000dc577857b7f2a5c18a4f2ce60196ea
tomlukrprogram <tomlukeprogramming@outlook.com>
2023-10-20 15:09:15 +0100

    working away

--- index.js
+++ index.js
@@ -4,16 +4,33 @@ const state = {
   tours: []
 }
 
-const root = ''
+
+const root = 'https://api.openbrewerydb.org/v1/breweries'
 const brewList = document.querySelector ('#breweries-list')
 const stateSelectForm = document.querySelector('#select-state-form')
 const filterByForm = document.querySelector('#filter-by-type-form')
 
+const renderTours = () => {
+
+}
+
+
 
 
 const renderStateSelectAndRender = () =>{
-fetch ('${root}')
-}
+fetch('${root}/state')
+.then((response) => response.json())
+    .then((data) => {
+      console.log("read state", data);
+      state.tours = data;
+      renderTours();
+    })
+    .catch(error => console.error('Error fetching Todos:', error));
+};
+
+// const listItem = document.createElement('li')
+
+
 // list should only show breweries that do tours
 
 //need to be able to view certain types of info
