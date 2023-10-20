f4647035dadb10b84bffdd40172d1ce3bb283471
Jack Miller <jdmiller1991@outlook.com>
2023-10-20 17:23:31 +0100

    Initial construction and planning

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const state = {
     breweries: []
 }
 
-
-
-
+const root = 'https://api.openbrewerydb.org/v1/breweries'
 
 // Query selectors / define the root
 
+select-state-section
+select-state
+breweries-list
+filter-by-type
 
 
 
-
-// Build a fetch function
+// Build a fetch function that works based on the state entered in search. Filter out all breweries that aren't Micro, Regional or Brewpub.
 
 
 
@@ -23,7 +24,22 @@ const state = {
 
 
 
-
+/* <li>
+  <h2>Snow Belt Brew</h2>
+  <div class="type">micro</div>
+  <section class="address">
+    <h3>Address:</h3>
+    <p>9511 Kile Rd</p>
+    <p><strong>Chardon, 44024</strong></p>
+  </section>
+  <section class="phone">
+    <h3>Phone:</h3>
+    <p>N/A</p>
+  </section>
+  <section class="link">
+    <a href="null" target="_blank">Visit Website</a>
+  </section>
+</li> */
 
 
 // Add an event listener. 
