d4d9a79cb95f3ee1be27569322bff07dceb6bbe6
tomlukrprogram <tomlukeprogramming@outlook.com>
2023-10-20 12:30:14 +0100

    started laying out code

---
+++ index.js
@@ -0,0 +1,22 @@
+// enter state and view breweries
+
+const state = {
+  tours: []
+}
+
+const root = ''
+const brewList = document.querySelector ('#breweries-list')
+const stateSelectForm = document.querySelector('#select-state-form')
+const filterByForm = document.querySelector('#filter-by-type-form')
+
+
+
+const renderStateSelectAndRender = () =>{
+fetch ('${root}')
+}
+// list should only show breweries that do tours
+
+//need to be able to view certain types of info
+
+// name, type, address, phone number
+
