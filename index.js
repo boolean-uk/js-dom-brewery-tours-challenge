a38ef1898c97cbfc83cbd052c4cbae1c165322ee
Lukas Dembicki <lad_777@hotmail.com>
2023-10-23 16:00:50 +0100

    Finished Extension 1

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const root = "https://api.openbrewerydb.org/v1/breweries";
 const breweryList = document.querySelector("#breweries-list");
 const selectStateForm = document.querySelector("#select-state-form");
 const filterByType = document.getElementById("filter-by-type");
-const article = document.querySelector("article")
+const article = document.querySelector("article");
 
 // Render brewery list function //
 const removeBreweries = () => {
@@ -20,79 +20,76 @@ const removeBreweries = () => {
     breweryListUl.forEach((child) => child.remove());
 };
 
+const renderSearchBar = () => {
+    const section = document.createElement("section");
 
-const renderSearch = () => {
-    const section = document.createElement('section')
+    const form = document.createElement("form");
+    form.setAttribute("id", "search-breweries-form");
 
-    const form = document.createElement ('form')
-    form.setAttribute("id", "search-breweries-form")
+    const label = document.createElement("label");
+    label.setAttribute("for", "searchBarInput");
+    form.append(label);
 
-    const label = document.createElement('label')
-    label.setAttribute("for", "searchBarInput")
-    form.append(label)
+    const h2 = document.createElement("h2");
+    h2.innerText = "Search Breweries:";
+    label.append(h2);
 
-    const h2 = document.createElement('h2')
-    h2.innerText = "Search Breweries:"
-    label.append(h2)
-    
-    const input = document.createElement('input')
-    input.setAttribute("type", "text")
-    input.setAttribute("id", "searchBarInput")
-    form.append(input)
+    const input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("id", "searchBarInput");
+    form.append(input);
 
-    section.append(form)
-    article.append(section)
+    section.append(form);
+    article.prepend(section);
+};
 
-}
-
-renderSearch()
 // The main render for the page based on value/state entered in search bar //
 const renderBreweryList = (breweryState) => {
     breweryState.breweries.forEach((brewery) => {
         const li = document.createElement("li");
-
+        
         const breweryName = document.createElement("h2");
         breweryName.innerText = brewery.name;
-
+        
         const breweryType = document.createElement("div");
         breweryType.innerText = brewery.brewery_type;
         breweryType.classList.add("type");
-
+        
         const addressSection = document.createElement("section");
         addressSection.classList.add("address");
-
+        
         const address = document.createElement("h3");
         address.innerText = "Address:";
 
         const street = document.createElement("p");
         street.innerText = brewery.street;
-
+        
         const cityAndPostCode = document.createElement("p");
         const strong = document.createElement("strong");
         strong.innerText = `${brewery.city}, ${brewery.postal_code}`;
         cityAndPostCode.append(strong);
-
+        
         addressSection.append(address, street, cityAndPostCode);
-
+        
         const phoneSection = document.createElement("section");
         phoneSection.classList = "phone";
-
+        
         const phoneNumberTitle = document.createElement("h3");
         phoneNumberTitle.innerText = "Phone:";
-
+        
         const phoneNumber = document.createElement("p");
         phoneNumber.innerText = brewery.phone;
-
+        
         phoneSection.append(phoneNumberTitle, phoneNumber);
-
+        
         const websiteLinkSection = document.createElement("section");
         websiteLinkSection.classList.add("link");
-
+        
         const linkAnchor = document.createElement("a");
         linkAnchor.setAttribute("href", brewery.website_url);
         linkAnchor.setAttribute("target", "_blank");
         linkAnchor.innerText = "Visit Website";
-
+        
         websiteLinkSection.append(linkAnchor);
 
         li.append(
@@ -101,7 +98,7 @@ const renderBreweryList = (breweryState) => {
             addressSection,
             phoneSection,
             websiteLinkSection
-        );
+            );
 
         breweryList.append(li);
     });
@@ -109,10 +106,10 @@ const renderBreweryList = (breweryState) => {
 // Event listener to populate state //
 selectStateForm.addEventListener("submit", (event) => {
     event.preventDefault();
-
+    
     const searchState = event.target[0].value;
     // console.log(searchState);
-
+    
     fetch(`${root}?by_state=${searchState}`)
         .then((response) => response.json())
         .then((data) => {
@@ -122,48 +119,64 @@ selectStateForm.addEventListener("submit", (event) => {
                     item.brewery_type === "micro" ||
                     item.brewery_type === "regional" ||
                     item.brewery_type === "brewpub"
-                ) {
+                    ) {
                     BreweryCanVisit.push(item);
                 }
                 return BreweryCanVisit;
             });
-
+            
             // console.log(BreweryCanVisit);
             state.breweries = BreweryCanVisit;
             removeBreweries();
             renderBreweryList(state);
         });
-});
-
-// Event listener that looks at different selcted options in dropdown list //
-filterByType.addEventListener("click", (event) => {
-    let selectedType = filterByType.value;
-
-    if (selectedType === "micro") {
-        console.log("micro")
+    });
+    
+    // Event listener that looks at different selcted options in dropdown list //
+    filterByType.addEventListener("click", (event) => {
+        let selectedType = filterByType.value;
+        
+        if (selectedType === "micro") {
+            console.log("micro");
         changeState("micro");
     } else if (selectedType === "regional") {
-        console.log("regional")
+        console.log("regional");
         changeState("regional");
     } else if (selectedType === "brewpub") {
-        console.log("brewpub")
+        console.log("brewpub");
         changeState("brewpub");
-    }else {
-        removeBreweries()
-        renderBreweryList(state)
+    } else {
+        removeBreweries();
+        renderBreweryList(state);
     }
 });
 
 // A function that chnages what shows on html based on what parameter is passed in //
 function changeState(type) {
-    newState.breweries = []
+    newState.breweries = [];
     state.breweries.forEach((brewery) => {
         if (brewery.brewery_type === type) {
             newState.breweries.push(brewery);
         }
         return newState;
     });
-    removeBreweries()
-    renderBreweryList(newState)
+    removeBreweries();
+    renderBreweryList(newState);
 }
 
+renderSearchBar();
+
+const searchBarInput = document.getElementById("searchBarInput");
+searchBarInput.addEventListener("input", (event) => {
+    
+    const inputedBrewery = event.target.value;
+    // console.log(inputedBrewery);
+    
+    fetch(`${root}?by_name=${inputedBrewery}`)
+    .then((response) => response.json())
+    .then((data) => {
+        state.breweries = data
+        removeBreweries()
+        renderBreweryList(state)
+    })
+});
