5ea344726cfc6777736938bfb11495d9f339f0e7
tomlukrprogram <tomlukeprogramming@outlook.com>
2023-10-24 17:20:36 +0100

    added formatting

--- index.js
+++ index.js
@@ -1,48 +1,40 @@
 // enter state and view breweries
 
 const state = {
-  tours: []
-}
+  tours: [],
+};
 
-
-const root = 'https://api.openbrewerydb.org/v1/breweries'
-const brewList = document.querySelector ('#breweries-list')
-const stateSelectForm = document.querySelector('#select-state-form')
-const filterByForm = document.querySelector('#filter-by-type-form')
-
-
+const root = "https://api.openbrewerydb.org/v1/breweries";
+const brewList = document.querySelector("#breweries-list");
+const stateSelectForm = document.querySelector("#select-state-form");
+const filterByForm = document.querySelector("#filter-by-type-form");
 
 const renderTours = () => {
+  const searchBreweriesByType = (type) => {
+    fetch(`${root}?by_type=${type}&per_page=3`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+  renderTours();
+};
 
-const searchBreweriesByType = (type) => {
-  fetch(`${root}?by_type=${type}&per_page=3`)
-   .then((response) => response.json())
-   .then((data) => {
-     console.log(data)
-   })
-}
-    renderTours();
-  }
-
-
-
-const renderStateSelectAndRender = () =>{
-fetch("${root}/state")
-.then((response) => response.json())
+const renderStateSelectAndRender = () => {
+  fetch("${root}/state")
+    .then((response) => response.json())
     .then((data) => {
       console.log("read state", data);
       state.tours = data;
       renderTours();
     })
-    .catch(error => console.error('Error fetching Todos:', error));
+    .catch((error) => console.error("Error fetching Todos:", error));
 };
 
 // const listItem = document.createElement('li')
-
 
 // list should only show breweries that do tours
 
 //need to be able to view certain types of info
 
 // name, type, address, phone number
-
