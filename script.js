edc3b16c29a39e740a4b80040aca08dbd141009b
Priya <Ps975076@gmail.com>
2023-10-26 16:30:05 +0100

    updated brewery type

--- script.js
+++ script.js
@@ -19,125 +19,127 @@
 // A user can enter a US state and view a list of breweries in that state.
 
 const state = {
-    breweries: [],
+  breweries: [],
 };
 
-
-const root = `https://api.openbrewerydb.org/v1/breweries`
-
+const root = `https://api.openbrewerydb.org/v1/breweries`;
 
 // For rendering breweries - select the unordered list
 // '#breweries-list' --> html page
-const breweriesListContainer = document.querySelector('#breweries-list')
-
+const breweriesListContainer = document.querySelector("#breweries-list");
 
 // READ - GET REQUEST
 
 function usState(baseUrl) {
-    fetch(`${baseUrl}`)
+  fetch(`${baseUrl}`)
     .then((reponse) => reponse.json())
-    .then((data)=> {
-        console.log("Data:",data)
-        state.breweries = data;
-        renderBreweries()
-    })
+    .then((data) => {
+      console.log("Data:", data);
+      state.breweries = data.filter(
+        (brewery) =>
+          brewery.brewery_type === "micro" ||
+          brewery.brewery_type === "regional" ||
+          brewery.brewery_type === "brewpub"
+      );
+      renderBreweries();
+    });
 }
-usState(root)
+usState(root);
 
 // Render breweries
 function renderBreweries() {
-    // Reset Container
-    breweriesListContainer.innerText = ""
+  // Reset Container
+  breweriesListContainer.innerText = "";
 
-    // Render
-    state.breweries.forEach((eachBrewery)=> {
-        // 1. LIs - Now create the elements within the brewery list, li elements
-        const breweryLi = document.createElement("li")
-        breweriesListContainer.append(breweryLi)
+  // Render
+  state.breweries.forEach((eachBrewery) => {
+    // 1. LIs - Now create the elements within the brewery list, li elements
+    const breweryLi = document.createElement("li");
+    breweriesListContainer.append(breweryLi);
 
-        // 2. Header
-        const breweryHeader = document.createElement("h2")
-        breweryHeader.innerText = eachBrewery.name
-        breweryLi.append(breweryHeader)
+    // 2. Header
+    const breweryHeader = document.createElement("h2");
+    breweryHeader.innerText = eachBrewery.name;
+    breweryLi.append(breweryHeader);
 
-        // 3. Div
-        const breweryDiv = document.createElement('div')
-        breweryDiv.setAttribute("class", "type")
-        breweryDiv.innerText = eachBrewery.brewery_type
-        breweryLi.append(breweryDiv)
+    // 3. Div
+    const breweryDiv = document.createElement("div");
+    breweryDiv.setAttribute("class", "type");
+    breweryDiv.innerText = eachBrewery.brewery_type;
+    breweryLi.append(breweryDiv);
 
-        //4. Address Section
-        const breweryAddressSection = document.createElement('section')
-        breweryAddressSection.setAttribute("class", "address")
-        breweryLi.append(breweryAddressSection)
+    //4. Address Section
+    const breweryAddressSection = document.createElement("section");
+    breweryAddressSection.setAttribute("class", "address");
+    breweryLi.append(breweryAddressSection);
 
-        //5. Within section: create elements
-        // 5a. h3 tag
-        const addressh3 = document.createElement('h3')
-        addressh3.innerText = "Address:"
-        breweryAddressSection.append(addressh3)
+    //5. Within section: create elements
+    // 5a. h3 tag
+    const addressh3 = document.createElement("h3");
+    addressh3.innerText = "Address:";
+    breweryAddressSection.append(addressh3);
 
-        //5b. p tag
-        const addressP = document.createElement('p')
-        addressP.innerText = eachBrewery.street
-        breweryAddressSection.append(addressP)
+    //5b. p tag
+    const addressP = document.createElement("p");
+    addressP.innerText = eachBrewery.street;
+    breweryAddressSection.append(addressP);
 
-        //5c. p tag2
-        const addressPTwo = document.createElement('p')
-        breweryAddressSection.append(addressPTwo)
-        const strongAddressP = document.createElement('strong')
-        addressPTwo.append(strongAddressP)
-        strongAddressP.innerText = `${eachBrewery.city }, ${eachBrewery.postal_code}`
-        
-        // 6. Phone Section
-        const phoneSection = document.createElement('section')
-        phoneSection.setAttribute("class", "phone")
-        breweryLi.append(phoneSection)
+    //5c. p tag2
+    const addressPTwo = document.createElement("p");
+    breweryAddressSection.append(addressPTwo);
+    const strongAddressP = document.createElement("strong");
+    addressPTwo.append(strongAddressP);
+    strongAddressP.innerText = `${eachBrewery.city}, ${eachBrewery.postal_code}`;
 
-        // 6a. h3 tag phone section
-        const phoneSectionh3 = document.createElement('h3')
-        phoneSectionh3.innerText = 'Phone:'
-        phoneSection.append(phoneSectionh3)
+    // 6. Phone Section
+    const phoneSection = document.createElement("section");
+    phoneSection.setAttribute("class", "phone");
+    breweryLi.append(phoneSection);
 
-        // 6b. p tag phone section
-        const phoneSectionP = document.createElement('p')
-        phoneSectionP.innerText = eachBrewery.phone
-        phoneSection.append(phoneSectionP)
+    // 6a. h3 tag phone section
+    const phoneSectionh3 = document.createElement("h3");
+    phoneSectionh3.innerText = "Phone:";
+    phoneSection.append(phoneSectionh3);
 
-        // 7. Link Section
-        const linkSection = document.createElement('section')
-        linkSection.setAttribute('class', 'link')
-        breweryLi.append(linkSection)
+    // 6b. p tag phone section
+    const phoneSectionP = document.createElement("p");
+    phoneSectionP.innerText = eachBrewery.phone;
+    phoneSection.append(phoneSectionP);
 
-        // 7a. Link a tag
-        const aSection = document.createElement('a')
-        aSection.setAttribute('href', eachBrewery.website_url)
-        aSection.setAttribute('target', '_blank')
-        aSection.innerText = 'Visit Website'
-        linkSection.append(aSection)
-        
-    })
+    // 7. Link Section
+    const linkSection = document.createElement("section");
+    linkSection.setAttribute("class", "link");
+    breweryLi.append(linkSection);
+
+    // 7a. Link a tag
+    const aSection = document.createElement("a");
+    aSection.setAttribute("href", eachBrewery.website_url);
+    aSection.setAttribute("target", "_blank");
+    aSection.innerText = "Visit Website";
+    linkSection.append(aSection);
+  });
 }
 
 // Search by State
-// When you search by US state, the type of brewery gets filtered by Micro, Regional and Brewpub 
+// When you search by US state, the type of brewery gets filtered by Micro, Regional and Brewpub
 
 //Search by State
 // Example, type in "new-york", all the breweries in new-york should come up, once filtered applied, this should change accordingly.
 
-
-// Select the search button 
+// Select the search button
 // Select the search input
 // Select the type of brew input
-const searchButton = document.querySelector('#select-state-form > input[type=submit]:nth-child(3)')
-const typeOfBrewInput = document.querySelector('#filter-by-type')
-const searchInput = document.querySelector('#select-state')
+const searchButton = document.querySelector(
+  "#select-state-form > input[type=submit]:nth-child(3)"
+);
+const typeOfBrewInput = document.querySelector("#filter-by-type");
+const searchInput = document.querySelector("#select-state");
 
-searchButton.addEventListener("click", (e)=> {
-    e.preventDefault()
-    // Call function
-    stateAndTypeAPIs() 
-})
+searchButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  // Call function
+  stateAndTypeAPIs();
+});
 
 //Filter breweries that offer brewery tours by:
 // 1. Micro
@@ -145,16 +147,21 @@ searchButton.addEventListener("click", (e)=> {
 // 3. Brewpub
 
 // Call function
-typeOfBrewInput.addEventListener('change', stateAndTypeAPIs)
+typeOfBrewInput.addEventListener("change", stateAndTypeAPIs);
 
 function stateAndTypeAPIs() {
-    // Get the value for the state (event listener)
-    const stateValue = searchInput.value
-    // Get the value for the type of brew (event listener)
-    const brewTypeValue = typeOfBrewInput.value
+  // Get the value for the state (event listener)
+  const stateValue = searchInput.value;
+  // Get the value for the type of brew (event listener)
+  const brewTypeValue = typeOfBrewInput.value;
 
+  let searchUrl = root + `?by_state=${stateValue}`;
+
+  // If statement to ensure that all breweries come up with the type Mirco, Regional and Brewpub when a state is entered:
+  if (brewTypeValue) {
     // Get the root, followed by the state and type of brewery API.
-    const searchURL = root+`?by_state=${stateValue}&by_type=${brewTypeValue}`
-    // API call and render
-    usState(searchURL)
+    searchUrl = root + `?by_state=${stateValue}&by_type=${brewTypeValue}`;
+  }
+  // API call and render
+  usState(searchUrl);
 }
