5f3c032210c3eb878cf2c9a4e463475d5f4dc307
tomlukrprogram <tomlukeprogramming@outlook.com>
2023-10-27 10:00:28 +0100

    added more

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 
-
 const state = {
   tours: [],
 };
 
-// selectors
 
 const root = "https://api.openbrewerydb.org/v1/breweries";
 const brewList = document.querySelector("#breweries-list");
 const stateSelectForm = document.querySelector("#select-state-form");
 const filterByForm = document.querySelector("#filter-by-type-form");
-const filterSection = document.querySelectorAll(".filters-section")[1];
-const article = document.querySelector('article')
+const filterByTypeSelect = document.querySelector("#filter-by-type");
+const article = document.querySelector('article');
 
-
+const renderBreweryDetails = (brewery) => {
+   alert(`Brewery Name: ${brewery.name}\nType: ${brewery.brewery_type}\nAddress: ${brewery.address}`);
+};
 
 const renderBreweries = (data) => {
-  // Clear previous brewery list
+ 
   while (brewList.firstChild) {
     brewList.removeChild(brewList.firstChild);
   }
   const breweryItem = document.createElement("li");
 
   data.forEach((brewery) => {
-    // Create elements for each brewery
+  
 
     const breweryName = document.createElement("h2");
     const breweryType = document.createElement("div");
@@ -52,54 +52,74 @@ const renderBreweries = (data) => {
     breweryItem.appendChild(sectionPhone);
     breweryItem.appendChild(sectionLink);
 
+
+    // Event listener to display brewery details when clicked
+    breweryItem.addEventListener("click", () => {
+      fetchBreweryDetails(brewery.id);
+    });
+
     brewList.appendChild(breweryItem);
   });
 };
+const fetchBreweryDetails = (breweryId) => {
+  const breweryDetailsEndpoint = `${root}/${breweryId}`;
 
-// search by type
-
-const renderTours = () => {
-  const searchBreweriesByType = (type) => {
-    fetch(`${root}?by_type=${type}&per_page=3`)
-      .then((response) => response.json())
-      .then((data) => {
-        renderBreweries(data);
-      })
-      .catch((error) => console.error("Error fetching Data:", error));
-  };
-
-  filterByForm.addEventListener("change", (event) => {
-    const selectedType = document.querySelector("#filter-by-type").value;
-    searchBreweriesByType(selectedType);
-  });
-  
-
-  const searchBreweriesByState = (states) => {
-    fetch(`${root}?by_state=${states}&per_page=3`)
-      .then((response) => response.json())
-      .then((data) => {
-        renderBreweries(data);
-      })
-      .catch((error) => console.error("Error fetching Data:", error));
-  };
-  stateSelectForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const selectedState = document.querySelector("#select-state").value;
-    searchBreweriesByState(selectedState);
-  });
-  renderTours();
+  fetch(breweryDetailsEndpoint)
+    .then((response) => response.json())
+    .then((brewery) => {
+      renderBreweryDetails(brewery);
+    })
+    .catch((error) => console.error("Error fetching brewery details:", error));
 };
 
 const renderStateSelectAndRender = () => {
-  fetch("${root}/state")
+  fetch(`${root}/state`)
     .then((response) => response.json())
     .then((data) => {
-      console.log("read state", data);
-      state.tours = data;
-      renderTours();
+      const stateSelect = document.querySelector("#select-state");
+      data.forEach((state) => {
+        const option = document.createElement("option");
+        option.value = state;
+        option.text = state;
+        stateSelect.appendChild(option);
+      });
+
+      filterByTypeSelect.addEventListener("change", () => {
+        const selectedType = filterByTypeSelect.value;
+        if (selectedType === "") {
+          const selectedState = stateSelect.value;
+          fetchBreweriesByState(selectedState);
+        } else {
+          fetchBreweriesByTypeAndState(selectedType);
+        }
+      });
     })
     .catch((error) => console.error("Error fetching Data:", error));
 };
 
+const fetchBreweriesByState = (selectedState) => {
+  fetch(`${root}?by_state=${selectedState}&per_page=3`)
+    .then((response) => response.json())
+    .then((data) => {
+      renderBreweries(data);
+    })
+    .catch((error) => console.error("Error fetching Data:", error));
+};
 
+const fetchBreweriesByTypeAndState = (selectedType) => {
+  const selectedState = stateSelectForm.value;
+  fetch(`${root}?by_type=${selectedType}&by_state=${selectedState}&per_page=3`)
+    .then((response) => response.json())
+    .then((data) => {
+      renderBreweries(data);
+    })
+    .catch((error) => console.error("Error fetching Data:", error));
+};
 
+stateSelectForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const selectedState = stateSelectForm.value;
+  fetchBreweriesByState(selectedState);
+});
+
+renderStateSelectAndRender();
