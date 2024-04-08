cb3c51183af64438bc3b82bde4771cab743091b2
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-04-08 12:06:21 +0200

    add function to render all brewery cards

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const selectStateForm = document.querySelector('#select-state-form')
 const selectStateInput = document.querySelector('#select-state')
+const breweryUl = document.querySelector('#breweries-list')
 
 async function getAllBreweries() {
     const response = await fetch(`https://api.openbrewerydb.org/v1/breweries?by_state=${selectStateInput.value}`)
     const data = await response.json()
 
-    console.log(data)
+    renderBreweryCards(data)
 }
 
 selectStateForm.addEventListener('submit', async (event) => {
@@ -13,3 +14,55 @@ selectStateForm.addEventListener('submit', async (event) => {
     await getAllBreweries()
 })
 
+function renderBreweryCards(data) {
+    breweryUl.innerHTML = ''
+
+    data.forEach((brewery) => {
+        const li = document.createElement('li')
+        const breweryName = document.createElement('h2')
+        const div = document.createElement('div')
+        const addressSection = document.createElement('section')
+        const addressH3 = document.createElement('h3')
+        const adressP = document.createElement('p')
+        const cityP = document.createElement('p')
+        const strong = document.createElement('strong')
+        const phoneSection = document.createElement('section')
+        const phoneH3 = document.createElement('h3')
+        const phoneP = document.createElement('p')
+        const websiteSection = document.createElement('section')
+        const websiteLink = document.createElement('a')
+
+        breweryName.innerText = brewery.name
+        div.classList.add('type')
+        div.innerText = brewery.brewery_type
+        addressSection.classList.add('address')
+        addressH3.innerText = 'Address:'
+        adressP.innerText = brewery.address_1
+        strong.innerText = brewery.city + ', ' + brewery.postal_code
+        phoneSection.classList.add('phone')
+        phoneH3.innerText = 'Phone:'
+        phoneP.innerText = brewery.phone
+        websiteSection.classList.add('link')
+        websiteLink.setAttribute('href', brewery.website_url)
+        websiteLink.setAttribute('target', '_blank')
+        websiteLink.innerText = 'Visit Website'
+
+        li.append(breweryName)
+        li.append(div)
+        li.append(addressSection)
+        li.append(phoneSection)
+        li.append(websiteSection)
+
+        addressSection.append(addressH3)
+        addressSection.append(adressP)
+        addressSection.append(cityP)
+        cityP.append(strong)
+
+        phoneSection.append(phoneH3)
+        phoneSection.append(phoneP)
+
+        websiteSection.append(websiteLink)
+
+        breweryUl.append(li)
+    });
+}
