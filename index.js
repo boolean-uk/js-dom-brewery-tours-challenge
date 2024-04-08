8ac0afb4ce58162870e08c8bcb5bb2322c8cfceb
Will Baxter <bax.will@googlemail.com>
2024-04-08 12:32:06 +0100

    Added filter functionality

--- index.js
+++ index.js
@@ -1,91 +1,103 @@
 //Build brewery card
 const buildCard = (brewery) => {
-    const breweryList = document.querySelector('#breweries-list')
+  const breweryList = document.querySelector("#breweries-list");
 
-    const li = document.createElement('li')
+  const li = document.createElement("li");
 
-    const h2 = document.createElement('h2')
-    h2.innerText = brewery.name
-    li.append(h2)
+  const h2 = document.createElement("h2");
+  h2.innerText = brewery.name;
+  li.append(h2);
 
-    const div = document.createElement('div')
-    div.innerText = brewery.brewery_type
-    div.classList.add('type')
-    li.append(div)
+  const div = document.createElement("div");
+  div.innerText = brewery.brewery_type;
+  div.classList.add("type");
+  li.append(div);
 
-    const addSection = document.createElement('section')
-    addSection.classList.add('address')
+  const addSection = document.createElement("section");
+  addSection.classList.add("address");
 
-    const addressH3 = document.createElement('h3')
-    addressH3.innerText = "Address:"
-    addSection.append(addressH3)
+  const addressH3 = document.createElement("h3");
+  addressH3.innerText = "Address:";
+  addSection.append(addressH3);
 
-    const line1P = document.createElement('p')
-    line1P.innerText = brewery.address_1
-    addSection.append(line1P)
+  const line1P = document.createElement("p");
+  line1P.innerText = brewery.address_1;
+  addSection.append(line1P);
 
-    const line2P = document.createElement('p')
-    const strongText = document.createElement('strong')
-    strongText.innerText = `${brewery.city}, ${brewery.postal_code}`
-    line2P.append(strongText)
-    addSection.append(line2P)
+  const line2P = document.createElement("p");
+  const strongText = document.createElement("strong");
+  strongText.innerText = `${brewery.city}, ${brewery.postal_code}`;
+  line2P.append(strongText);
+  addSection.append(line2P);
 
-    li.append(addSection)
+  li.append(addSection);
 
-    const phoneSection = document.createElement('section')
-    phoneSection.classList.add('phone')
+  const phoneSection = document.createElement("section");
+  phoneSection.classList.add("phone");
 
-    const phoneH3 = document.createElement('h3')
-    phoneH3.innerText = 'Phone:'
-    phoneSection.append(phoneH3)
+  const phoneH3 = document.createElement("h3");
+  phoneH3.innerText = "Phone:";
+  phoneSection.append(phoneH3);
 
-    const phoneP = document.createElement('p')
-    if (brewery.phone) {
-        phoneP.innerText = brewery.phone
-    } else {
-        phoneP.innerText = "N/A"
-    }
-    phoneSection.append(phoneP)
+  const phoneP = document.createElement("p");
+  if (brewery.phone) {
+    phoneP.innerText = brewery.phone;
+  } else {
+    phoneP.innerText = "N/A";
+  }
+  phoneSection.append(phoneP);
 
-    li.append(phoneSection)
+  li.append(phoneSection);
 
-    if (brewery.website_url) {
-        const linkSection = document.createElement('section')
-        linkSection.classList.add('link')
-        const link = document.createElement('a')
-        link.innerText = 'Visit Website'
-        link.setAttribute('target', '_blank')
-        link.setAttribute('href', brewery.website_url)
-        linkSection.append(link)
-        li.append(linkSection)
-    }
+  if (brewery.website_url) {
+    const linkSection = document.createElement("section");
+    linkSection.classList.add("link");
+    const link = document.createElement("a");
+    link.innerText = "Visit Website";
+    link.setAttribute("target", "_blank");
+    link.setAttribute("href", brewery.website_url);
+    linkSection.append(link);
+    li.append(linkSection);
+  }
 
-    breweryList.append(li)
-}
+  breweryList.append(li);
+};
 
+//Render cards
+const render = async (state, type) => {
+  const breweryList = document.querySelector("#breweries-list");
+  breweryList.innerHTML = "";
 
-//Render cards by state
-const render = (state) => {
-    const breweryList = document.querySelector('#breweries-list')
-    breweryList.innerHTML = ''
+  const defaultBreweryTypes = ["micro", "regional", "brewpub"];
+    console.log(`${state}, ${type}`)
+  if (!type) {
+    defaultBreweryTypes.forEach(async (element) => {
+      const data = await fetch(
+        `https://api.openbrewerydb.org/v1/breweries?by_state=${state}&by_type=${element}`
+      );
+      const json = await data.json();
+      json.forEach((element) => {buildCard(element)});
+    });
+  } else {
+    const data = await fetch(
+        `https://api.openbrewerydb.org/v1/breweries?by_state=${state}&by_type=${type}`
+      );
+      const json = await data.json();
+      json.forEach((element) => {buildCard(element)});
+  }
+};
 
-    const breweryTypes = ['micro', 'regional', 'brewpub']
+//Get search state input and filter input then trigger render
+const searchForm = document.querySelector("#select-state-form");
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-    breweryTypes.forEach(async (element) => {
-        const data = await fetch(`https://api.openbrewerydb.org/v1/breweries?by_state=${state}&by_type=${element}`)
-        const json = await data.json()
-        json.forEach(buildCard)
-})
+  const searchInput = document.querySelector("#select-state").value;
 
-
-}
-
-//Get search state input and trigger render
-const searchForm = document.querySelector('#select-state-form')
-searchForm.addEventListener('submit', (event) => {
-    event.preventDefault()
-    
-    const searchInput = document.querySelector('#select-state').value
-
-    render(searchInput)
-})
+  const typeFilter = document.querySelector("#filter-by-type");
+  if (typeFilter.value.length > 0) {
+    render(searchInput, typeFilter.value);
+  } else {
+    render(searchInput);
+  }
+});
