707114e94ac1d8afa42bef1641d7f56cbc99391b
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-04-09 14:59:10 +0200

    change event listeners

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Imports 
+// HTML imports 
 const selectStateForm = document.querySelector('#select-state-form')
 const selectStateInput = document.querySelector('#select-state')
 const breweryUl = document.querySelector('#breweries-list')
@@ -80,6 +80,15 @@ searchBreweries.addEventListener('keyup', () => {
     currentPage = 1
 })
 
+clearAllButton.addEventListener('click', () => {
+    checkedCities = []
+    getAllBreweries()
+})
+
+filterByCityForm.addEventListener('change', () => {
+    getAllBreweries()
+})
+
 // Render lists
 function renderBreweryCards(data) {
     breweryUl.innerHTML = ''
@@ -155,19 +164,14 @@ function renderCities(uniqueCities) {
 
         clearAllButton.addEventListener('click', () => {
             checkbox.checked = false
-            checkedCities = []
-            getAllBreweries()
         })
 
         filterByCityForm.addEventListener('change', () => {
             if (checkbox.checked && checkedCities.indexOf(city) === -1) {
                 checkedCities.push(city)
-                console.log(checkedCities)
             } else if (!checkbox.checked && checkedCities.indexOf(city) >= 0) {
                 checkedCities.splice(checkedCities.findIndex(cityItem => cityItem === city), 1)
             }
-
-            getAllBreweries()
         })
     })
 }
@@ -213,121 +217,3 @@ function createUniqueCityList(data) {
 
 // Call functions
 getAllBreweries()
-
-// function renderBreweryCards(data) {
-//     breweryUl.innerHTML = ''
-
-//     data.forEach((brewery) => {
-//         const li = document.createElement('li')
-//         const breweryName = document.createElement('h2')
-//         const div = document.createElement('div')
-//         const addressSection = document.createElement('section')
-//         const addressH3 = document.createElement('h3')
-//         const adressP = document.createElement('p')
-//         const cityP = document.createElement('p')
-//         const strong = document.createElement('strong')
-//         const phoneSection = document.createElement('section')
-//         const phoneH3 = document.createElement('h3')
-//         const phoneP = document.createElement('p')
-//         const websiteSection = document.createElement('section')
-//         const websiteLink = document.createElement('a')
-
-//         breweryName.innerText = brewery.name
-//         div.classList.add('type')
-//         div.innerText = brewery.brewery_type
-//         addressSection.classList.add('address')
-//         addressH3.innerText = 'Address:'
-//         adressP.innerText = brewery.address_1
-//         strong.innerText = brewery.city + ', ' + brewery.postal_code
-//         phoneSection.classList.add('phone')
-//         phoneH3.innerText = 'Phone:'
-//         phoneP.innerText = brewery.phone
-//         websiteSection.classList.add('link')
-//         websiteLink.setAttribute('href', brewery.website_url)
-//         websiteLink.setAttribute('target', '_blank')
-//         websiteLink.innerText = 'Visit Website'
-
-//         li.append(breweryName)
-//         li.append(div)
-//         li.append(addressSection)
-//         li.append(phoneSection)
-//         li.append(websiteSection)
-
-//         addressSection.append(addressH3)
-//         addressSection.append(adressP)
-//         addressSection.append(cityP)
-//         cityP.append(strong)
-
-//         phoneSection.append(phoneH3)
-//         phoneSection.append(phoneP)
-
-//         websiteSection.append(websiteLink)
-
-//         breweryUl.append(li)
-//     });
-//     renderPagination()
-// }
-
-// async function getAllBreweries() {
-//     const response = await fetch(`https://api.openbrewerydb.org/v1/breweries?`)
-//     const data = await response.json()
-
-//     const filteredData = data.filter((brewery) => {
-//         if (brewery.brewery_type === 'micro'|| brewery.brewery_type === 'brewpub' || brewery.brewery_type === 'regional') return brewery
-//     })
-
-//     totalPages = Math.ceil(filteredData.length / itemsPerPage)
-
-//     renderBreweryCards(filteredData)
-//     createUniqueCityList(filteredData)
-// }
-
-// async function getAllBreweriesFilteredByCity() {
-//     const response = await fetch(`https://api.openbrewerydb.org/v1/breweries?by_state=${selectStateInput.value}&by_name=${searchBreweries.value}`)
-//     const data = await response.json()
-    
-//     const filteredData = data.filter((brewery) => {
-//         if (brewery.brewery_type === 'micro'|| brewery.brewery_type === 'brewpub' || brewery.brewery_type === 'regional') return brewery
-//     })
-
-//     const filteredByCity = filteredData.filter((brew) => {
-//         if (checkedCities.includes(brew.city)) return brew
-//     })
-
-//     if (filteredByCity.length === 0) {
-//         renderBreweryCards(filteredData)
-//         totalPages = Math.ceil(filteredData.length / itemsPerPage)
-//     } else {
-//         renderBreweryCards(filteredByCity)
-//         totalPages = Math.ceil(filteredByCity.length / itemsPerPage)
-//     }
-
-// }
-
-// async function getAllBreweriesFiltered() {
-//     if (selectBreweryType.value === undefined || selectBreweryType.value === '') {
-//         const response = await fetch(`https://api.openbrewerydb.org/v1/breweries?by_state=${selectStateInput.value}&by_name=${searchBreweries.value}`)
-//         const data = await response.json()
-    
-//         const filteredData = data.filter((brewery) => {
-//             if (brewery.brewery_type === 'micro'|| brewery.brewery_type === 'brewpub' || brewery.brewery_type === 'regional') return brewery
-//         })
-
-//         renderBreweryCards(filteredData)
-//         createUniqueCityList(filteredData)
-
-//         totalPages = Math.ceil(filteredData.length / itemsPerPage)
-//         renderPagination()
-
-//     } else {
-//         const response = await fetch(`https://api.openbrewerydb.org/v1/breweries?by_state=${selectStateInput.value}&by_type=${selectBreweryType.value}&by_name=${searchBreweries.value}`)
-//         const data = await response.json()
-
-//         renderBreweryCards(data)
-//         createUniqueCityList(data)
-
-//         totalPages = Math.ceil(data.length / itemsPerPage)
-//         renderPagination()
-//     }
-
-// }
